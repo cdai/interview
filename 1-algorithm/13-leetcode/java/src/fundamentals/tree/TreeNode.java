a4e711837502ddabdf9a9eec5377259ad46104b6
cdai <dc_726@163.com>
2016-06-20 22:47:40 -0400

    leetcode-144 binary tree preorder traversal

---
+++ 1-algorithm/13-leetcode/java/src/fundamentals/tree/TreeNode.java
@@ -0,0 +1,8 @@
+package fundamentals.tree;
+
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val) { this.val = val; }
+}
---
+++ 1-algorithm/13-leetcode/java/src/fundamentals/tree/lc144_binarytreepreordertraversal/Solution.java
@@ -0,0 +1,38 @@
+package fundamentals.tree.lc144_binarytreepreordertraversal;
+
+import fundamentals.tree.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * Given a binary tree, return the preorder traversal of its nodes' values.
+ * For example: Given binary tree {1,#,2,3}, return [1,2,3].
+ */
+public class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> vals = new ArrayList<>();
+        if (root == null) {
+            return vals;
+        }
+
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+
+            // Pre-orderly visit current node
+            vals.add(node.val);
+
+            if (node.right != null) { // error2: push right first so make left popped first
+                stack.push(node.right);
+            }
+
+            if (node.left != null) { // error1: push null into stack by mistake causing NPE
+                stack.push(node.left);
+            }
+        }
+        return vals;
+    }
+}
