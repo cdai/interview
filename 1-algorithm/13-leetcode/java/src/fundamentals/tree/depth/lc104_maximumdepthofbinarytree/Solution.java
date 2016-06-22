0585bae3815cff5f890b34cce05c54e546d96096
cdai <dc_726@163.com>
2016-06-21 21:03:53 -0400

    leetcode-104 Maximum depth of binary tree

---
+++ 1-algorithm/13-leetcode/java/src/fundamentals/tree/depth/lc104_maximumdepthofbinarytree/Solution.java
@@ -0,0 +1,29 @@
+package fundamentals.tree.depth.lc104_maximumdepthofbinarytree;
+
+import fundamentals.tree.TreeNode;
+
+/**
+ * Given a binary tree, find its maximum depth.
+ * The maximum depth is the number of nodes along the longest path
+ * from the root node down to the farthest leaf node.
+ */
+public class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return doMaxDepth(root, 1);
+    }
+
+    private int doMaxDepth(TreeNode root, int depth) {
+        int lDepth = depth;
+        int rDepth = depth;
+        if (root.left != null) {
+            lDepth = doMaxDepth(root.left, depth+1);
+        }
+        if (root.right != null) {
+            rDepth = doMaxDepth(root.right, depth+1);
+        }
+        return Math.max(lDepth, rDepth);
+    }
+}
