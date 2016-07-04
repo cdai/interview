f07e69115cdc8a1e8ae54e28cffa2234286f58a7
cdai <dc_726@163.com>
2016-07-04 16:57:28 -0400

    leetcode-007 reverse number

---
+++ 1-algorithm/13-leetcode/java/src/misc/math/lc007_reverseinteger/Solution.java
@@ -0,0 +1,23 @@
+package misc.math.lc007_reverseinteger;
+
+/**
+ * Reverse digits of an integer.
+ *  Example1: x = 123, return 321
+ *  Example2: x = -123, return -321
+ */
+public class Solution {
+
+    public int reverse(int x) {
+        int r = 0;
+        while (x != 0) {
+            if (r > Integer.MAX_VALUE / 10 || r < Integer.MIN_VALUE / 10) {
+                return 0;
+            }
+
+            r = r * 10 + x % 10;
+            x = x / 10;
+        }
+        return r;
+    }
+
+}
