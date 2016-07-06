f53dcb9290a44e2dc1b2323b2fa29ad14610276a
cdai <dc_726@163.com>
2016-07-06 14:59:48 -0400

    leetcode-021 merge two sorted lists

---
+++ 1-algorithm/13-leetcode/java/src/buildingblock/sorting/lc021_mergetwosortedlists/Solution.java
@@ -0,0 +1,30 @@
+package buildingblock.sorting.lc021_mergetwosortedlists;
+
+import fundamentals.list.ListNode;
+
+/**
+ * Merge two sorted linked lists and return it as a new list.
+ * The new list should be made by splicing together the nodes of the first two lists.
+ */
+public class Solution {
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode first = new ListNode(-1);
+        ListNode prev = first;
+        while (l1 != null || l2 != null) {
+            int d1 = (l1 == null) ? Integer.MAX_VALUE : l1.val;
+            int d2 = (l2 == null) ? Integer.MAX_VALUE : l2.val;
+
+            if (d1 < d2) {     // error: l1, l2 would not be moved together!
+                prev.next = l1;
+                l1 = l1.next;
+            } else {
+                prev.next = l2;
+                l2 = l2.next;
+            }
+            prev = prev.next;
+        }
+        return first.next;
+    }
+
+}
