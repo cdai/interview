2b04c7bf0761621ea5e2ad3cdad95fe3d7fc4759
cdai <dc_726@163.com>
2016-07-06 22:29:09 -0400

    leetcode-069 sqrt

---
+++ 1-algorithm/13-leetcode/java/src/buildingblock/binarysearch/lc069_sqrt/Solution.java
@@ -0,0 +1,52 @@
+package buildingblock.binarysearch.lc069_sqrt;
+
+/**
+ * Sqrt(x)
+ */
+public class Solution {
+
+    public int mySqrt(int x) {
+        if (x < 2) {
+            return x;
+        }
+
+        int low = 1, high = x / 2;
+        int last = 0;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (x / mid > mid) {
+                low = mid + 1;
+                last = mid;
+            } else if (x / mid < mid) {
+                high = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return last;
+    }
+
+    public int mySqrt1(int x) {
+        if (x <= 1) {
+            return x;
+        }
+
+        int low = 0, high = x;
+        do {
+            int mid = (low + high) / 2;
+            if (Integer.MAX_VALUE / mid < mid) {
+                high = mid;
+                continue;
+            }
+
+            int sqr = mid * mid;
+            if (sqr < x) {
+                low = mid;
+            } else {
+                high = mid;
+            }
+        } while (low < high);
+        return low;
+    }
+
+}
