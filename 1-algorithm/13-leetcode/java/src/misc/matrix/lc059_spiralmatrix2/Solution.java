bb9412d6ae8259e486fc6cd58acd22965cad720b
cdai <dc_726@163.com>
2016-07-19 15:32:33 -0400

    leetcode-059 spiral matrix 2

--- 1-algorithm/13-leetcode/java/src/misc/matrix/lc059_spiralmatrix2/Solution.java
+++ 1-algorithm/13-leetcode/java/src/misc/matrix/lc059_spiralmatrix2/Solution.java
@@ -1,4 +1,4 @@
-package misc.matrix.lc059_spiralmatrix;
+package misc.matrix.lc059_spiralmatrix2;
 
 /**
  * Given an integer n, generate a square matrix filled with elements from 1 to n2 in spiral order.
