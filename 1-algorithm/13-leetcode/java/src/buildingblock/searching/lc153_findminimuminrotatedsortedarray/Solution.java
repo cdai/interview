e36c6d732e20da0b4f15d13896488554732252b7
cdai <dc_726@163.com>
2016-07-24 16:31:06 -0400

    leetcode-153 find minimum in rotated sorted array

---
+++ 1-algorithm/13-leetcode/java/src/buildingblock/searching/lc153_findminimuminrotatedsortedarray/Solution.java
@@ -0,0 +1,66 @@
+package buildingblock.searching.lc153_findminimuminrotatedsortedarray;
+
+/**
+ * Suppose a sorted array is rotated at some pivot unknown to you beforehand.
+ * (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2). Find the minimum element.
+ * You may assume no duplicate exists in the array.
+ */
+public class Solution {
+
+    // Test case:
+    // 1.Odd or even number of values
+    // 2.Min is first, last or first half or second half
+    // 3.Empty, only-one array
+    // 4.positive, negative, zero
+    public int findMin(int[] nums) {
+        /* MustBe(0,N-1): min value must be in range [0,N-1] */
+        int low = 0, high = nums.length - 1;
+
+        /* MustBe(low,high): (1) when initialization, invariant holds */
+        while (low < high) {
+
+            /* MustBe(low,high) and low < high */
+            int mid = (low + high) / 2;
+
+            /* Since at least mid = (low + low + 1) / 2 = low */
+            /* => MustBe(low,high) and low <= mid < high */
+            if (nums[mid] < nums[high]) {
+
+                /* Since nums[mid]<...<mid[high]: Pivot is not in (mid,high] (but could be nums[mid])*/
+                /* => MustBe(low,mid) */
+                high = mid;
+            } else {
+                /* Since symmetrical as above */
+                /* => MustBe(mid,high) */
+                if (low == mid) {
+                    /* MustBe(low,high) and low + 1 = high: (3) when termination, invariant holds */
+                    return Math.min(nums[low], nums[high]);
+                }
+
+                /* MustBe(mid,high) */
+                low = mid;
+            }
+
+            /* MustBe(low,high): (2) when preservation, invariant holds */
+        }
+
+        /* Since invariant holds when intialization, preservation and termination */
+        /* => MustBe(low,high) and low == high (if low > high, MustBe(low,high) assertion would fail) */
+        return nums[low];
+    }
+
+    /* Very casual version without solid thought of correctness */
+    public int findMin2(int[] nums) {
+        int low = 0, high = nums.length - 1;
+        while (low < high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] < nums[high]) { // right half is correct, so min is in left half
+                high = (mid == high) ? mid - 1 : mid;
+            } else {
+                low = (mid == low) ? mid + 1 : mid;
+            }
+        }
+        return nums[low]; // outofbounds if nums is null or empty
+    }
+
+}
