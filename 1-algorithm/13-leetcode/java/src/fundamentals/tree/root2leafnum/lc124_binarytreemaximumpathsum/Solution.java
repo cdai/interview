013abfd0171483a9786d736331210c0efb97e0a0
cdai <dc_726@163.com>
2016-07-27 16:24:45 -0400

    leetcode-124 binary tree maximum path sum

--- 1-algorithm/13-leetcode/java/src/fundamentals/tree/root2leafnum/lc124_binarytreemaximumpathsum/Solution.java
+++ 1-algorithm/13-leetcode/java/src/fundamentals/tree/root2leafnum/lc124_binarytreemaximumpathsum/Solution.java
@@ -3,7 +3,9 @@ package fundamentals.tree.root2leafnum.lc124_binarytreemaximumpathsum;
 import fundamentals.tree.TreeNode;
 
 /**
- *
+ * Given a binary tree, find the maximum path sum. For this problem, a path is defined as any sequence of nodes
+ * from some starting node to any node in the tree along the parent-child connections.
+ * The path does not need to go through the root.
  */
 public class Solution {
 
@@ -16,7 +18,7 @@ public class Solution {
         System.out.println(new Solution().maxPathSum(root));
     }
 
-    private int maxSum = 0;
+    private int maxSum = Integer.MIN_VALUE; // error1: init to MIN_VAL, not 0. eg.[-3] -> -3 not 0
 
     public int maxPathSum(TreeNode root) {
         doMaxPathSum(root);
@@ -25,11 +27,32 @@ public class Solution {
 
     private int doMaxPathSum(TreeNode root) {
         if (root == null) {
-            return 0;
+            return 0;   // error2: it's ok, since we compare root+left,root+right, never left or right alone
         }
 
         int maxLeft = doMaxPathSum(root.left);
         int maxRight = doMaxPathSum(root.right);
+
+        // Max path sum that is useable by parent: root, root+left, root+right
+        int maxSumForParent = Math.max(root.val, root.val + maxLeft);
+        maxSumForParent = Math.max(maxSumForParent, root.val + maxRight);
+        maxSum = Math.max(maxSum, maxSumForParent);
+
+        // Mat path sum that is NOT useable by parent
+        maxSum = Math.max(maxSum, maxLeft + maxRight + root.val);
+
+        // Only return path sum reusable by parent
+        return maxSumForParent;
+    }
+
+    // Correct, but only works for positive nums
+    private int doMaxPathSum2(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int maxLeft = doMaxPathSum2(root.left);
+        int maxRight = doMaxPathSum2(root.right);
 
         // Update max path sum
         maxSum = Math.max(maxSum, maxLeft + maxRight + root.val);
