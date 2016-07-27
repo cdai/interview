580c237a65f9f4f8a04099171a95bfefb63ff228
cdai <dc_726@163.com>
2016-07-27 19:43:15 -0400

    leetcode-037 sudoku solver

---
+++ 1-algorithm/13-leetcode/java/src/advanced/combinatorial/backtracking/dfs/lc037_sudokusolver/Solution.java
@@ -0,0 +1,100 @@
+package advanced.combinatorial.backtracking.dfs.lc037_sudokusolver;
+
+import java.util.Arrays;
+
+/**
+ * Write a program to solve a Sudoku puzzle by filling the empty cells.
+ * Empty cells are indicated by the character '.'. You may assume that there will be only one unique solution.
+ */
+public class Solution {
+
+    public void solveSudoku(char[][] board) {
+        doSolve(board, 0, 0);
+    }
+
+    private boolean doSolve(char[][] board, int row, int col) {
+        // Find next empty cell
+        int[] empty = findNextEmptyCell(board, row, col);
+        row = empty[0];
+        col = empty[1];
+
+        // Reach out of board, find solution!
+        if (row == board.length) {
+            return true;
+        }
+
+        // Try each candidate
+        for (char c : candidates(board, row, col)) {
+            board[row][col] = c;
+            if (doSolve(board, row, col + 1)) {
+                return true;
+            }
+            board[row][col] = '.';
+        }
+        return false;
+    }
+
+    private int[] findNextEmptyCell(char[][] board, int row, int col) {
+        for ( ; row < board.length; row++) {
+            for ( ; col < board[row].length; col++) {
+                if (board[row][col] == '.') {
+                    return new int[]{ row, col };
+                }
+            }
+            col = 0;
+        }
+        return new int[]{ row, col };
+    }
+
+    private char[] candidates(char[][] board, int row, int col) {
+        boolean[] candidates = new boolean[10];
+        Arrays.fill(candidates, true);
+        int total = 9;
+
+        // Scan row
+        for (int i = 0; i < board[row].length; i++) {
+            char c = board[row][i];
+            if (c != '.') {
+                if (candidates[c - '0']) {
+                    total--;
+                    candidates[c - '0'] = false;
+                }
+            }
+        }
+
+        // Scan column
+        for (int i = 0; i < board.length; i++) {
+            char c = board[i][col];
+            if (c != '.') {
+                if (candidates[c - '0']) {
+                    total--;
+                    candidates[c - '0'] = false;
+                }
+            }
+        }
+
+        // Scan square
+        int sqrRow = (row / 3) * 3;
+        int sqrCol = (col / 3) * 3;
+        for (int i = sqrRow; i < sqrRow + 3; i++) {
+            for (int j = sqrCol; j < sqrCol + 3; j++) {
+                char c = board[i][j];
+                if (c != '.') {
+                    if (candidates[c - '0']) {
+                        total--;
+                        candidates[c - '0'] = false;
+                    }
+                }
+            }
+        }
+
+        char[] result = new char[total];
+        for (int i = 1, j = 0; i < candidates.length; i++) {
+            if (candidates[i]) {
+                result[j++] = (char) (i + '0');
+            }
+        }
+        return result;
+    }
+
+}
