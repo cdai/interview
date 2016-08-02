578c8637d4cddd54b35e6da4f2cae9b74836d512
cdai <dc_726@163.com>
2016-08-02 09:48:20 -0400

    leetcode-238 product of array except self

---
+++ 1-algorithm/13-leetcode/java/src/advanced/scan/twopasses/lc238_productofarrayexceptself/Solution.java
@@ -0,0 +1,33 @@
+package advanced.scan.twopasses.lc238_productofarrayexceptself;
+
+import java.util.Arrays;
+
+/**
+ * Given an array of n integers where n > 1, nums, return an array output such that output[i]
+ * is equal to the product of all the elements of nums except nums[i]. Solve it without division and in O(n).
+ * For example, given [1,2,3,4], return [24,12,8,6].
+ * Follow up: Could you solve it with constant space complexity?
+ * (Note: The output array does not count as extra space for the purpose of space complexity analysis.)
+ */
+public class Solution {
+
+    public int[] productExceptSelf(int[] nums) {
+        int[] prod = new int[nums.length];
+        Arrays.fill(prod, 1);
+
+        // Invariant: sum(prod[i]) = product of number [0,i-1]
+        for (int i = 1, sum = 1; i < nums.length; i++) {
+            sum *= nums[i - 1];
+            prod[i] *= sum;
+        }
+
+        // Invariant: sum = product of number [i+1,len-1]
+        // prod[i] = product of number [0,i-1] * [i+1,len-1]
+        for (int i = nums.length - 2, sum = 1; i >= 0; i--) {
+            sum *= nums[i + 1];
+            prod[i] *= sum;
+        }
+        return prod;
+    }
+
+}
