bfe78ed661034236ea57c64138193008d041db4b
cdai <dc_726@163.com>
2016-08-05 10:42:38 -0400

    leetcode-374 guess number higher or lower

---
+++ 1-algorithm/13-leetcode/java/src/buildingblock/searching/lc374_guessnumberhigherorlower/GuessGame.java
@@ -0,0 +1,11 @@
+package buildingblock.searching.lc374_guessnumberhigherorlower;
+
+/**
+ */
+public class GuessGame {
+
+    protected int guess(int n) {
+        return 0;
+    }
+
+}
---
+++ 1-algorithm/13-leetcode/java/src/buildingblock/searching/lc374_guessnumberhigherorlower/Solution.java
@@ -0,0 +1,32 @@
+package buildingblock.searching.lc374_guessnumberhigherorlower;
+
+/**
+ * We are playing the Guess Game. The game is as follows: I pick a number from 1 to n.
+ * You have to guess which number I picked. Every time you guess wrong,
+ * I'll tell you whether the number is higher or lower.
+ * You call a pre-defined API guess(int num) which returns 3 possible results (-1, 1, or 0):
+ *  -1 : My number is lower
+ *  1 : My number is higher
+ *  0 : Congrats! You got it!
+ * Example: n = 10, I pick 6. Return 6.
+ */
+public class Solution extends GuessGame {
+
+    public int guessNumber(int n) {
+        return doGuess(1, n);
+    }
+
+    private int doGuess(int low, int high) {
+        //int mid = (low + high) / 2;
+        int mid = low + ((high - low) / 2); // error: avoid overflow!!! [2126753390,1702766719]
+        int guess = guess(mid);
+        if (guess < 0) {
+            return doGuess(low, mid - 1);
+        } else if (guess > 0) {
+            return doGuess(mid + 1, high);
+        } else {
+            return mid;
+        }
+    }
+
+}
