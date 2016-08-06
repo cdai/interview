f16410ec820f5efb3c7b410e6fc271ca720b5949
cdai <dc_726@163.com>
2016-08-06 14:20:51 -0400

    leetcode-303 range sum query immutable

---
+++ 1-algorithm/13-leetcode/java/src/advanced/scan/singlepointer/lc303_rangesumqueryimmutable/NumArray.java
@@ -0,0 +1,36 @@
+package advanced.scan.singlepointer.lc303_rangesumqueryimmutable;
+
+/**
+ * Given an integer array nums, find the sum of the elements between indices i and j (i ≤ j), inclusive.
+ * Example: Given nums = [-2, 0, 3, -5, 2, -1]
+ *  sumRange(0, 2) -> 1
+ *  sumRange(2, 5) -> -1
+ *  sumRange(0, 5) -> -3
+ * Note: You may assume that the array does not change. There are many calls to sumRange function.
+ */
+public class NumArray {
+
+    // sum[i] = sum of [0,i]
+    private int[] sum;
+
+    public NumArray(int[] nums) {
+        if (nums.length == 0) {
+            sum = new int[0];
+            return;
+        }
+
+        sum = new int[nums.length];
+        sum[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            sum[i] = sum[i - 1] + nums[i];
+        }
+    }
+
+    public int sumRange(int i, int j) {
+        if (i < 0 || j >= sum.length) {
+            return 0;
+        }
+        return sum[j] - (i > 0 ? sum[i - 1] : 0);
+    }
+
+}
---
+++ 1-algorithm/13-leetcode/java/src/advanced/scan/singlepointer/lc303_rangesumqueryimmutable/NumArray2.java
@@ -0,0 +1,28 @@
+package advanced.scan.singlepointer.lc303_rangesumqueryimmutable;
+
+/**
+ * sumRange() is O(1), but totally TLE, since matrix could be very huge!
+ */
+public class NumArray2 {
+
+    // sum[i][j] = sum of [i,j]
+    // note: only right half of matrix is available
+    private int[][] sum;
+
+    public NumArray2(int[] nums) {
+        sum = new int[nums.length][nums.length];
+        for (int i = 0; i < sum.length; i++) {
+            sum[i][i] = nums[i];
+        }
+        for (int i = 0; i < sum.length; i++) {
+            for (int j = i + 1; j < sum[i].length; j++) {
+                sum[i][j] = sum[i][j - 1] + nums[j];
+            }
+        }
+    }
+
+    public int sumRange(int i, int j) {
+        return sum[i][j];
+    }
+
+}
