8f68f9f519ff18969ebf57ee5b08ed7418b76471
cdai <dc_726@163.com>
2016-08-05 22:35:38 -0400

    leetcode-278 first bad version

---
+++ 1-algorithm/13-leetcode/java/src/buildingblock/searching/lc278_firstbadversion/Solution.java
@@ -0,0 +1,32 @@
+package buildingblock.searching.lc278_firstbadversion;
+
+/**
+ * You are a product manager and currently leading a team to develop a new product.
+ * Unfortunately, the latest version of your product fails the quality check.
+ * Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+ * Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one,
+ * which causes all the following ones to be bad.
+ * You are given an API bool isBadVersion(version) which will return whether version is bad.
+ * Implement a function to find the first bad version. You should minimize the number of calls to the API.
+ */
+public class Solution extends VersionControl {
+
+    public int firstBadVersion(int n) {
+        /* MustBe(low,high) */
+        int low = 0, high = n;
+
+        /* MustBe(0,n) */
+        while (low < high) { // low<=high -> low<=mid<=high below causing dead loop
+            int mid = low + (high - low) / 2;
+
+            /* low <= mid < high */
+            if (isBadVersion(mid)) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+
+}
---
+++ 1-algorithm/13-leetcode/java/src/buildingblock/searching/lc278_firstbadversion/VersionControl.java
@@ -0,0 +1,9 @@
+package buildingblock.searching.lc278_firstbadversion;
+
+public class VersionControl {
+
+    protected boolean isBadVersion(int version) {
+        return true;
+    }
+
+}
