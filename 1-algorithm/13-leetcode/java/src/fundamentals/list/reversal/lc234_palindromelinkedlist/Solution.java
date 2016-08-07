9970e6e8033278912316515080cc6560b6b12a8c
cdai <dc_726@163.com>
2016-08-07 15:10:04 -0400

    leetcode-274 palindrome linked list

---
+++ 1-algorithm/13-leetcode/java/src/fundamentals/list/reversal/lc234_palindromelinkedlist/Solution.java
@@ -0,0 +1,65 @@
+package fundamentals.list.reversal.lc234_palindromelinkedlist;
+
+import fundamentals.list.ListNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Given a singly linked list, determine if it is a palindrome.
+ * Follow up: Could you do it in O(n) time and O(1) space?
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(-16557);
+        head.next = new ListNode(-16557);
+        System.out.println(new Solution().isPalindrome(head));
+    }
+
+    public boolean isPalindrome(ListNode head) {
+        if (head == null) {
+            return true;
+        }
+
+        // 1.Find middle node
+        ListNode mid = head, fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            mid = mid.next;
+            fast = fast.next.next;
+        }
+
+        // 2.Reverse second half
+        ListNode prev = mid, cur = mid.next;
+        while (cur != null) {
+            ListNode tmp = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = tmp;
+        }
+
+        // 3.Check palindrome
+        for (ListNode left = head, right = prev; right != mid; left = left.next, right = right.next) { // error: dead loop if only check left!=right
+            if (left.val != right.val) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // O(N) space
+    public boolean isPalindrome2(ListNode head) {
+        List<Integer> vals = new ArrayList<>();
+        for (ListNode cur = head; cur != null; cur = cur.next) {
+            vals.add(cur.val);
+        }
+
+        for (int i = 0, j = vals.size() - 1; i < j; i++, j--) {
+            if (!vals.get(i).equals(vals.get(j))) { // error: != is wrong, use equals...
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
