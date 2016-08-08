ae2beb447b724999e552cedcd264f83f5a710408
cdai <dc_726@163.com>
2016-08-08 10:26:42 -0400

    leetcode-306 additive number

--- 1-algorithm/13-leetcode/java/src/fundamentals/string/arithmetic/lc306_additivenumber/Solution.java
+++ 1-algorithm/13-leetcode/java/src/fundamentals/string/arithmetic/lc306_additivenumber/Solution.java
@@ -1,6 +1,14 @@
 package fundamentals.string.arithmetic.lc306_additivenumber;
 
 /**
+ * Additive number is a string whose digits can form additive sequence.
+ * A valid additive sequence should contain at least three numbers.
+ * Except for the first two numbers, each subsequent number in the sequence must be the sum of the preceding two.
+ * For example: "112358" is an additive number because the digits can form an additive sequence: 1, 1, 2, 3, 5, 8. 1 + 1 = 2, 1 + 2 = 3, 2 + 3 = 5, 3 + 5 = 8
+ * "199100199" is also an additive number, the additive sequence is: 1, 99, 100, 199. 1 + 99 = 100, 99 + 100 = 199.
+ * Note: Numbers in the additive sequence cannot have leading zeros, so sequence 1, 2, 03 or 1, 02, 3 is invalid.
+ * Given a string containing only digits '0'-'9', write a function to determine if it's an additive number.
+ * Follow up: How would you handle overflow for very large input integers?
  */
 public class Solution {
 
@@ -25,14 +33,15 @@ public class Solution {
                 }
 
                 StringBuilder str = new StringBuilder();
+                String prev = first, cur = second;          // error4: do NOT modify outer variable "first", use another tmp variable
                 while (str.length() < remaining.length()) {
-                    String third = add(first, second);
+                    String third = add(prev, cur);
                     str.append(third);
-                    first = second;
-                    second = third;
+                    prev = cur;
+                    cur = third;
                 }
 
-                if (str.toString().equals(remaining)) { // error2: must str.toString() before using equals for StringBuilder
+                if (str.toString().equals(remaining)) {     // error1: must str.toString() before using equals for StringBuilder
                     return true;
                 }
             }
@@ -55,6 +64,5 @@ public class Solution {
         }
         return result.toString();
     }
-
 
 }
