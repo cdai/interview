522bf24172b934defa7e1859442f1fe98faa5098
cdai <dc_726@163.com>
2016-08-09 16:43:01 -0400

    leetcode-211 add and search word

---
+++ 1-algorithm/13-leetcode/java/src/advanced/design/lc211_addandsearchword/TrieNode.java
@@ -0,0 +1,22 @@
+package advanced.design.lc211_addandsearchword;
+
+class TrieNode {
+
+    final TrieNode[] children;
+
+    boolean isWord;
+
+    char val;
+
+    // Initialize your data structure here.
+    public TrieNode() {
+        this.children = new TrieNode[26];
+        this.isWord = false;
+    }
+
+    public TrieNode(char c) {
+        this();
+        this.val = c;
+    }
+
+}
