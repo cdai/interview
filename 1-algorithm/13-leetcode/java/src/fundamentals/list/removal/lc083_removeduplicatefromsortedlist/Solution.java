77bf3060a76e0065b15ba9d20519e90ea9dc7d46
cdai <dc_726@163.com>
2016-08-10 19:01:57 -0400

    leetcode-083 remove duplicate from sorted list

--- 1-algorithm/13-leetcode/java/src/fundamentals/list/removal/lc083_removeduplicatefromsortedlist/Solution.java
+++ 1-algorithm/13-leetcode/java/src/fundamentals/list/removal/lc083_removeduplicatefromsortedlist/Solution.java
@@ -9,7 +9,48 @@ import fundamentals.list.ListNode;
  * Given 1->1->2->3->3, return 1->2->3.
  */
 public class Solution {
+
+    // Version-1: Compare cur and cur.next without prev
+    // Note both cur and cur.next could reach null
     public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+
+        ListNode cur = head;
+        while (cur != null && cur.next != null) {
+            if (cur.val == cur.next.val) {
+                cur.next = cur.next.next;
+            } else {
+                cur = cur.next;
+            }
+        }
+        return head;
+    }
+
+    // Version-2: compare cur and prev
+    // Note update of prev and cur
+    public ListNode deleteDuplicates2(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+
+        // Invariant: node prior to prev (inclusive) has no duplicates
+        ListNode cur = head.next, prev = head;
+        while (cur != null) {
+            if (cur.val == prev.val) {
+                prev.next = cur.next;
+                cur = prev.next;
+            } else {
+                prev = cur;
+                cur = cur.next;
+            }
+        }
+        return head;
+    }
+
+    // My 1st: prev != null could avoid, see 2nd solution above
+    public ListNode deleteDuplicates3(ListNode head) {
         if (head == null) {
             return head;
         }
