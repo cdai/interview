443186f02efa750f19dd96b15ec09b0568b2f284
cdai <dc_726@163.com>
2016-08-10 20:33:59 -0400

    leetcode-082 remove duplicate from sorted list 2

--- 1-algorithm/13-leetcode/java/src/fundamentals/list/removal/lc082_removeduplicatefromsortedlist2/Solution.java
+++ 1-algorithm/13-leetcode/java/src/fundamentals/list/removal/lc082_removeduplicatefromsortedlist2/Solution.java
@@ -10,7 +10,61 @@ import fundamentals.list.ListNode;
  * Given 1->1->1->2->3, return 2->3.
  */
 public class Solution {
+
+    // Solution from programcreek
     public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        // Invariant: nodes before prev (inclusive) have no duplicates
+        ListNode prev = dummy;
+        while (prev.next != null && prev.next.next != null) {
+            if (prev.next.val == prev.next.next.val) {
+                int dup = prev.next.val;
+                while (prev.next != null && prev.next.val == dup) { // delete all duplicates
+                    prev.next = prev.next.next;
+                }
+            } else {
+                prev = prev.next;
+            }
+        }
+        return dummy.next;
+    }
+
+    // My 2nd: a little trivial in loop body
+    public ListNode deleteDuplicates2(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        // Invariant: nodes before prev (inclusive) have no duplicates
+        ListNode prev = dummy;
+        while (prev != null && prev.next != null) {
+            // Find next distinct value
+            ListNode cur = prev.next;
+            ListNode dup = cur.next;
+            while (dup != null && cur.val == dup.val) {
+                dup = dup.next;
+            }
+
+            // dup is a node or null (reach end)
+            if (dup == cur.next) {  // no duplicates, delete none, just move forward
+                prev = prev.next;
+            } else {                // found duplicates, bypass all of them
+                prev.next = dup;
+            }
+        }
+        return dummy.next;
+    }
+
+    public ListNode deleteDuplicates3(ListNode head) {
         if (head == null) {
             return head;
         }
