422de4d163b70e89eb3f57449b3a09068c97b55b
cdai <dc_726@163.com>
2016-08-10 22:22:07 -0400

    leetcode-141 linked list cycle

--- 1-algorithm/13-leetcode/java/src/fundamentals/list/twopointers/lc141_linkedlistcycle/Solution.java
+++ 1-algorithm/13-leetcode/java/src/fundamentals/list/twopointers/lc141_linkedlistcycle/Solution.java
@@ -8,9 +8,10 @@ import fundamentals.list.ListNode;
  */
 public class Solution {
 
+    // My 2nd
     public boolean hasCycle(ListNode head) {
         ListNode fast = head, slow = head;
-        while (fast != null && fast.next != null) {
+        while (fast != null && fast.next != null) { // no need to check slow
             slow = slow.next;
             fast = fast.next.next;
             if (fast == slow) {     // Error: it's ok to put behind, since they will encounter anyway if cycle exists
