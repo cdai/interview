90dc0382dfd4fd64d92c5b30583a2802933e0e97
cdai <dc_726@163.com>
2016-08-11 15:59:01 -0400

    leetcode-144 binary tree preorder traversal

--- 1-algorithm/13-leetcode/java/src/fundamentals/tree/traversal/normal/lc144_binarytreepreordertraversal/Solution.java
+++ 1-algorithm/13-leetcode/java/src/fundamentals/tree/traversal/normal/lc144_binarytreepreordertraversal/Solution.java
@@ -3,6 +3,7 @@ package fundamentals.tree.traversal.normal.lc144_binarytreepreordertraversal;
 import fundamentals.tree.TreeNode;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Stack;
 
@@ -11,7 +12,82 @@ import java.util.Stack;
  * For example: Given binary tree {1,#,2,3}, return [1,2,3].
  */
 public class Solution {
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        System.out.println(new Solution().preorderTraversal(root));
+    }
+
+    // My 2nd: morris traversal. O(n) time, O(1) space
     public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        TreeNode cur = root;
+        while (cur != null) {
+            if (cur.left == null) {
+                result.add(cur.val);        // Exit-1: no left child, go right directly
+                cur = cur.right;
+            } else {
+                // Find rightmost in left subtree
+                TreeNode rightmost = cur.left;
+                while (rightmost.right != null && rightmost.right != cur) {
+                    rightmost = rightmost.right;
+                }
+
+                // 1) rightmost = null: then link i to root as footprint before go left
+                // 2) rightmost = cur: means we're back to root by footprint, so restore rightmost.right then go right
+                if (rightmost.right == null) {
+                    result.add(cur.val);    // Exit-2: leaving footprint, be about to visit left subtree soon
+                    rightmost.right = cur;
+                    cur = cur.left;
+                } else {
+                    rightmost.right = null;
+                    cur = cur.right;
+                }
+            }
+        }
+        return result;
+    }
+
+    // My 2nd: iterative using stack. O(n) time, O(h) space
+    public List<Integer> preorderTraversal_stack(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        if (root != null) {
+            stack.push(root);
+        }
+
+        List<Integer> result = new ArrayList<>();
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            result.add(node.val);
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+        }
+        return result;
+    }
+
+    // My 2nd: recursion. O(n) time, O(h) space
+    public List<Integer> preorderTraversal_recursion(TreeNode root) {
+        if (root == null) {
+            return Collections.emptyList();
+        }
+
+        List<Integer> result = new ArrayList<>();
+        result.add(root.val);
+        result.addAll(preorderTraversal(root.left));
+        result.addAll(preorderTraversal(root.right));
+        return result;
+    }
+
+    // My 1st
+    public List<Integer> preorderTraversal1(TreeNode root) {
         List<Integer> vals = new ArrayList<>();
         if (root == null) {
             return vals;
