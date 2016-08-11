a170c5bed7c9077cc4722f8f90dd0a881ddf5e46
cdai <dc_726@163.com>
2016-08-11 16:50:39 -0400

    leetcode-107 binary tree level order traversal 2

--- 1-algorithm/13-leetcode/java/src/fundamentals/tree/traversal/level/lc107_binarytreelevelordertraversal2/Solution.java
+++ 1-algorithm/13-leetcode/java/src/fundamentals/tree/traversal/level/lc107_binarytreelevelordertraversal2/Solution.java
@@ -2,6 +2,7 @@ package fundamentals.tree.traversal.level.lc107_binarytreelevelordertraversal2;
 
 import fundamentals.tree.TreeNode;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
@@ -18,7 +19,55 @@ import java.util.Queue;
  * ]
  */
 public class Solution {
+
+    // DFS solution inspired by solution from leetcode discuss
     public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        doLevelOrderBottom(result, root, 1);
+        return result;
+    }
+
+    private void doLevelOrderBottom(List<List<Integer>> result, TreeNode root, int height) {
+        if (root == null) {
+            return;
+        }
+
+        if (result.size() < height) {
+            result.add(0, new ArrayList<>());
+        }
+        result.get(result.size() - height).add(root.val);   // Note that: size() - height
+        doLevelOrderBottom(result, root.left, height + 1);
+        doLevelOrderBottom(result, root.right, height + 1);
+    }
+
+    // My 2nd: BFS solution with only one line changed
+    public List<List<Integer>> levelOrderBottom2(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        if (root != null) {
+            queue.offer(root);
+        }
+
+        List<List<Integer>> result = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> level = new ArrayList<>(size);
+            while (size-- > 0) {
+                TreeNode node = queue.poll();
+                level.add(node.val);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            result.add(0, level); // Only difference!!!
+        }
+        return result;
+    }
+
+    // My 1st
+    public List<List<Integer>> levelOrderBottom1(TreeNode root) {
         List<List<Integer>> vals = new LinkedList<>();
         if (root == null) {
             return vals;
