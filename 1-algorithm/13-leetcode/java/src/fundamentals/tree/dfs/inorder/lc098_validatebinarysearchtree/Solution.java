4545795a2838cacf118d6f52d2a51e1ed7562469
cdai <dc_726@163.com>
2016-08-12 12:46:46 -0400

    leetcode-098 validate binary search tree

--- 1-algorithm/13-leetcode/java/src/fundamentals/tree/dfs/inorder/lc098_validatebinarysearchtree/Solution.java
+++ 1-algorithm/13-leetcode/java/src/fundamentals/tree/dfs/inorder/lc098_validatebinarysearchtree/Solution.java
@@ -4,6 +4,7 @@ import fundamentals.tree.TreeNode;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * Given a binary tree, determine if it is a valid binary search tree (BST).
@@ -14,7 +15,68 @@ import java.util.List;
  */
 public class Solution {
 
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(2);
+        root.left = new TreeNode(1);
+        root.right = new TreeNode(3);
+        System.out.println(new Solution().isValidBST(root));
+    }
+
+    // Correct recursive version from leetcode discuss
     public boolean isValidBST(TreeNode root) {
+        return isValidBST(Long.MIN_VALUE, root, Long.MAX_VALUE);
+    }
+
+    private boolean isValidBST(long low, TreeNode root, long high) {
+        if (root == null) {
+            return true;
+        }
+        return (low < root.val && root.val < high)
+                && isValidBST(low, root.left, root.val)
+                && isValidBST(root.val, root.right, high);
+    }
+
+    // My 2nd: use stack to traverse inorder iteratively.
+    public boolean isValidBST_iterative(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = root;
+        TreeNode prev = null;                               // error2: TreeNode nor int set to INT_MIN, since eg.[-2147483648]
+
+        while (!stack.isEmpty() || cur != null) {
+            if (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            } else {
+                cur = stack.pop();
+
+                // Inorder visit
+                if (prev != null && prev.val >= cur.val) {  // error1: must >=, eg.[1,1] is invalid tree too
+                    return false;
+                }
+                prev = cur;
+
+                cur = cur.right;
+            }
+        }
+        return true;
+    }
+
+    // This is wrong, you must use a range
+    public boolean isValidBST_recursion2(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        if (root.left != null && root.left.val >= root.val) {
+            return false;
+        }
+        if (root.right != null && root.right.val <= root.val) {
+            return false;
+        }
+        return isValidBST(root.left) && isValidBST(root.right);
+    }
+
+    // My 1st: slow because the whole inorder result is generated
+    public boolean isValidBST1(TreeNode root) {
         List<Integer> vals = new ArrayList<>();
 
         doValidate(vals, root);
