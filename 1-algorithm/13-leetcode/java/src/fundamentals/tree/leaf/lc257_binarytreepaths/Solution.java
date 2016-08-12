c844a2e4a1376f6956fd0325d8ebf2057926d37d
cdai <dc_726@163.com>
2016-08-12 09:15:10 -0400

    leetcode-257 binary tree paths

--- 1-algorithm/13-leetcode/java/src/fundamentals/tree/leaf/lc257_binarytreepaths/Solution.java
+++ 1-algorithm/13-leetcode/java/src/fundamentals/tree/leaf/lc257_binarytreepaths/Solution.java
@@ -3,6 +3,7 @@ package fundamentals.tree.leaf.lc257_binarytreepaths;
 import fundamentals.tree.TreeNode;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -16,8 +17,29 @@ import java.util.List;
  * All root-to-leaf paths are: ["1->2->5", "1->3"]
  */
 public class Solution {
-
+    
+    // My 2nd: use return value to replace result argument
     public List<String> binaryTreePaths(TreeNode root) {
+        return doFindPath(root, "");
+    }
+
+    private List<String> doFindPath(TreeNode root, String path) {
+        if (root == null) {
+            return Collections.emptyList();
+        }
+
+        List<String> result = new ArrayList<>();
+        if (root.left == null && root.right == null) {
+            result.add(path + root.val);
+        } else {
+            result.addAll(doFindPath(root.left, path + root.val + "->"));
+            result.addAll(doFindPath(root.right, path + root.val + "->"));
+        }
+        return result;
+    }
+
+    // My 1st
+    public List<String> binaryTreePaths1(TreeNode root) {
         List<String> result = new ArrayList<>();
         traverse(result, "", root);
         return result;
