b96e49665f2f8a2a74941894cb5335e09a8de162
cdai <dc_726@163.com>
2016-08-12 17:30:38 -0400

    leetcode-114 flatten binary tree to linked list

--- 1-algorithm/13-leetcode/java/src/fundamentals/tree/construct/lc114_flattenbinarytreetolinkedlist/Solution.java
+++ 1-algorithm/13-leetcode/java/src/fundamentals/tree/construct/lc114_flattenbinarytreetolinkedlist/Solution.java
@@ -26,17 +26,60 @@ import fundamentals.tree.TreeNode;
  */
 public class Solution {
 
+    // Freaking beatiful solution by reversed post-order traversal.
+    // Inspired by leetcode discuss.
     public void flatten(TreeNode root) {
+        doFlatten(root, null);
+    }
+
+    private TreeNode doFlatten(TreeNode root, TreeNode prev) {
+        if (root == null) {
+            return prev;
+        }
+        prev = doFlatten(root.right, prev);
+        prev = doFlatten(root.left, prev);
+
+        // prev is actually the last one in order
+        root.right = prev;
+        root.left = null;
+        prev = root;
+        return prev;
+    }
+
+    // My 2nd
+    public void flatten2(TreeNode root) {
         doFlatten(root);
     }
 
-    private TreeNode doFlatten(TreeNode node) {
+    private TreeNode doFlatten(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        TreeNode leftEnd = doFlatten(root.left);
+        TreeNode rightEnd = doFlatten(root.right);
+
+        if (leftEnd != null) {
+            leftEnd.right = root.right;
+            root.right = root.left;
+            root.left = null;                           // error1: clean left pointer
+        }
+        return rightEnd != null ? rightEnd :
+                ((leftEnd != null) ? leftEnd : root);   // error2: return itself for leaf
+    }
+
+    // My 1st
+    public void flatten1(TreeNode root) {
+        doFlatten(root);
+    }
+
+    private TreeNode doFlatten1(TreeNode node) {
         if (node == null) {
             return null;
         }
 
-        TreeNode rightmostInLeft = doFlatten(node.left);
-        TreeNode rightmostInRight = doFlatten(node.right);
+        TreeNode rightmostInLeft = doFlatten1(node.left);
+        TreeNode rightmostInRight = doFlatten1(node.right);
 
         // This is a leaf!
         if (rightmostInLeft == null && rightmostInRight == null) {
