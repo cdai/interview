32da7907ced69f555e80d2e45058991510d12add
cdai <dc_726@163.com>
2016-08-12 15:47:07 -0400

    leetcode-106 construct binary tree from inorder and post order traversal

--- 1-algorithm/13-leetcode/java/src/fundamentals/tree/construct/lc106_constructbinarytreefrominorderandpostordertraversal/Solution.java
+++ 1-algorithm/13-leetcode/java/src/fundamentals/tree/construct/lc106_constructbinarytreefrominorderandpostordertraversal/Solution.java
@@ -8,7 +8,33 @@ import fundamentals.tree.TreeNode;
  */
 public class Solution {
 
+    // My 2nd
     public TreeNode buildTree(int[] inorder, int[] postorder) {
+        return doBuildTree(inorder, 0, inorder.length - 1, postorder, 0, postorder.length - 1);
+    }
+
+    public TreeNode doBuildTree(int[] inorder, int inStart, int inEnd,
+                                int[] postorder, int postStart, int postEnd) {
+        if (inStart > inEnd) {
+            return null;
+        }
+
+        int rootVal = postorder[postEnd];
+        int i = inStart;
+        for (; i <= inEnd; i++) { // By changing to backward traversal it became ~1-2ms. It seems like the test cases are biased toward left subtrees being larger.
+            if (inorder[i] == rootVal) {
+                break;
+            }
+        }
+
+        TreeNode root = new TreeNode(rootVal);
+        root.left = doBuildTree(inorder, inStart, i - 1, postorder, postStart, postStart + (i - inStart) - 1);
+        root.right = doBuildTree(inorder, i + 1, inEnd, postorder, postStart + (i - inStart), postEnd - 1);
+        return root;
+    }
+
+    // My 1st
+    public TreeNode buildTree1(int[] inorder, int[] postorder) {
         if (inorder.length == 0 || postorder.length == 0 || inorder.length != postorder.length) {
             return null;
         }
