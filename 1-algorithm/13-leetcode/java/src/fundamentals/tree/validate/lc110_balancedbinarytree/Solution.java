9bea224fa056795ba4eb9d269e1ac4fd61449b66
cdai <dc_726@163.com>
2016-08-12 11:21:59 -0400

    leetcode-110 balanced binary tree

--- 1-algorithm/13-leetcode/java/src/fundamentals/tree/validate/lc110_balancedbinarytree/Solution.java
+++ 1-algorithm/13-leetcode/java/src/fundamentals/tree/validate/lc110_balancedbinarytree/Solution.java
@@ -8,7 +8,32 @@ import fundamentals.tree.TreeNode;
  * in which the depth of the two subtrees of every node never differ by more than 1.
  */
 public class Solution {
+
+    // My 2nd
     public boolean isBalanced(TreeNode root) {
+        return doCheckBalance(root) != -1;
+    }
+
+    // return -1 to terminate, otherwise it means height
+    private int doCheckBalance(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        // Only check if necessary (short circuit)
+        int h1 = doCheckBalance(root.left);
+        if (h1 == -1) {
+            return -1;
+        }
+        int h2 = doCheckBalance(root.right);
+        if (h2 == -1) {
+            return -1;
+        }
+        return Math.abs(h1 - h2) <= 1 ? Math.max(h1, h2) + 1 : -1;
+    }
+
+    // My 1st
+    public boolean isBalanced1(TreeNode root) {
         return (height(root) != -1);
     }
 
