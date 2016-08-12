41a9f74c1d162c15ef24c20035fee22bea9d47f1
cdai <dc_726@163.com>
2016-08-12 16:04:41 -0400

    leetcode-108 convert sorted array to binary search tree

--- 1-algorithm/13-leetcode/java/src/buildingblock/divideandconquer/lc108_convertsortedarraytobinarysearchtree/Solution.java
+++ 1-algorithm/13-leetcode/java/src/buildingblock/divideandconquer/lc108_convertsortedarraytobinarysearchtree/Solution.java
@@ -1,4 +1,4 @@
-package fundamentals.tree.construct.lc108_convertsortedarraytobinarysearchtree;
+package buildingblock.divideandconquer.lc108_convertsortedarraytobinarysearchtree;
 
 import fundamentals.tree.TreeNode;
 
@@ -7,7 +7,29 @@ import fundamentals.tree.TreeNode;
  */
 public class Solution {
 
+    // My 2nd: O(N) time, O(logN) space
     public TreeNode sortedArrayToBST(int[] nums) {
+        if (nums.length == 0) {
+            return null;
+        }
+        return doConvertToBST(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode doConvertToBST(int[] nums, int start, int end) {
+        if (start > end) {
+            return null;
+        }
+
+        int mid = start + (end - start) / 2;
+
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = doConvertToBST(nums, start, mid - 1);
+        root.right = doConvertToBST(nums, mid + 1, end);
+        return root;
+    }
+
+    // My 1st
+    public TreeNode sortedArrayToBST1(int[] nums) {
         if (nums == null || nums.length == 0) {
             return null;
         }
@@ -15,7 +37,7 @@ public class Solution {
     }
 
     private TreeNode doSortedArrayToBST(int[] nums, int low, int high) {
-        if (low == high) {
+        if (low == high) {          // Could speed up, but not a must
             return new TreeNode(nums[low]);
         } else if (low > high)  {
             return null;
