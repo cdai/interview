d21f2c643abb513fd2f43c21baac334f8b7c010b
cdai <dc_726@163.com>
2016-08-12 22:46:07 -0400

    leetcode-035 search insert position

--- 1-algorithm/13-leetcode/java/src/buildingblock/searching/lc035_searchinsertposition/Solution.java
+++ 1-algorithm/13-leetcode/java/src/buildingblock/searching/lc035_searchinsertposition/Solution.java
@@ -12,7 +12,49 @@ package buildingblock.searching.lc035_searchinsertposition;
  */
 public class Solution {
 
+    // My 2nd: O(logN) with detailed assertion
     public int searchInsert(int[] nums, int target) {
+        if (nums.length == 0) {
+            return 0;
+        }
+
+        // MustBe(0,n-1)
+        int low = 0, high = nums.length - 1;
+
+        // MustBe(low,high): (1) Initialization: invariant holds
+        while (low <= high) {
+
+            // MustBe(low,high) and low <= high
+            int mid = low + (high - low) / 2;
+
+            // MustBe(low,high) and low <= mid <= high
+            if (nums[mid] < target) {
+
+                // MustBe(low,high) and num[mid] < target <= num[high]
+                // MustBe(mid+1,high)
+                low = mid + 1;
+
+                // MustBe(low,high): (2) Preservation: invariant holds
+            } else if (nums[mid] > target) {
+
+                // MustBe(low,high) and num[low] <= target < num[mid]
+                // MustBe(low,mid-1)
+                high = mid - 1;
+
+                // MustBe(low,high)
+            } else {
+                return mid;
+            }
+        } // (3) Termination: range shrinks, so it must terminate
+
+        // low > high, so it was low = high = mid (it's impossible to get low > high if low < high)
+        // 1) target < num[mid], low=mid, high=mid-1, low is the insert position
+        // 2) target > num[mid], high=mid, low=mid+1, low is the insert position too!
+        return low;
+    }
+
+    // My 1st
+    public int searchInsert1(int[] nums, int target) {
         if (nums == null || nums.length == 0) {
             return 0;
         }
