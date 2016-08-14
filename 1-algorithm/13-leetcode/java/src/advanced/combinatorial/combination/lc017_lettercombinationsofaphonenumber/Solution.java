3bdd16f9082d2827c0b6196d50e70d6c7837b32d
cdai <dc_726@163.com>
2016-08-14 15:23:59 -0400

    leetcode-017 letter combinations of a phone number

--- 1-algorithm/13-leetcode/java/src/advanced/combinatorial/combination/lc017_lettercombinationsofaphonenumber/Solution.java
+++ 1-algorithm/13-leetcode/java/src/advanced/combinatorial/combination/lc017_lettercombinationsofaphonenumber/Solution.java
@@ -3,7 +3,9 @@ package advanced.combinatorial.combination.lc017_lettercombinationsofaphonenumbe
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * Given a digit string, return all possible letter combinations that the number could represent.
@@ -13,7 +15,66 @@ import java.util.List;
  */
 public class Solution {
 
+    // BFS solution from leetcode discuss
+    // DFS and BFS are uniformed between Tree and Combinatorial algorithm!
     public List<String> letterCombinations(String digits) {
+        final String[] map = { "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
+        Queue<String> queue = new LinkedList<>();
+        if (!digits.isEmpty()) {
+            queue.offer("");
+        }
+
+        // For tree, there're only two possiblities: left and right child.
+        // For this problem, there're many according to digits first char
+        while (!queue.isEmpty() && !digits.isEmpty()) {
+            int size = queue.size();
+            while (size-- > 0) {
+                String str = queue.poll();
+                for (char c : map[digits.charAt(0) - '0'].toCharArray()) {
+                    queue.offer(str + c);
+                }
+            }
+            digits = digits.substring(1);
+        };
+        return (List<String>) queue;
+    }
+
+    // My 2nd: this map could be replaced by string array
+    private static char[][] map = {
+            {},     // 0
+            {},     // 1
+            {'a', 'b', 'c'},
+            {'d', 'e', 'f'},
+            {'g', 'h', 'i'},
+            {'j', 'k', 'l'},
+            {'m', 'n', 'o'},
+            {'p', 'q', 'r', 's'},
+            {'t', 'u', 'v'},
+            {'w', 'x', 'y', 'z'}
+    };
+
+    public List<String> letterCombinations2(String digits) {
+        if (digits.isEmpty()) {
+            return new ArrayList<>();
+        }
+        return doLetterCombinations(new char[digits.length()], digits);
+    }
+
+    private List<String> doLetterCombinations(char[] path, String digits) { // Simplify: reduce result and k
+        if (digits.isEmpty()) {
+            return Arrays.asList(new String(path));
+        }
+
+        List<String> result = new ArrayList<>();
+        for (char c : map[digits.charAt(0) - '0']) {
+            path[path.length - digits.length()] = c;
+            result.addAll(doLetterCombinations(path, digits.substring(1)));
+        }
+        return result;
+    }
+
+    // My 1st: comb(n) = all possible letters at n * comb(n-1)
+    public List<String> letterCombinations1(String digits) {
         if (digits.length() == 0) {
             return Collections.emptyList();
         }
