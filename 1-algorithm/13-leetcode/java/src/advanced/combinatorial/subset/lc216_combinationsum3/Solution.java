20e2d129e9650d3a93b9d485b42d0546f3249e51
cdai <dc_726@163.com>
2016-08-14 22:08:02 -0400

    leetcode-216 combination sum 3

--- 1-algorithm/13-leetcode/java/src/advanced/combinatorial/subset/lc216_combinationsum3/Solution.java
+++ 1-algorithm/13-leetcode/java/src/advanced/combinatorial/subset/lc216_combinationsum3/Solution.java
@@ -11,7 +11,31 @@ import java.util.List;
  */
 public class Solution {
 
+    // My 2nd: O(9!) time, O(K) space
     public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> result = new ArrayList<>();
+        doCombineSum(result, new ArrayList<>(), k, n, 1);
+        return result;
+    }
+
+    private void doCombineSum(List<List<Integer>> result,
+                              List<Integer> path, int k, int n, int start) {
+        if (path.size() == k || n <= 0) {
+            if (path.size() == k && n == 0) {
+                result.add(new ArrayList<>(path));
+            }
+            return;
+        }
+
+        for (int i = start; i <= 9; i++) {
+            path.add(i);
+            doCombineSum(result, path, k, n - i, i + 1);
+            path.remove(path.size() - 1);
+        }
+    }
+
+    // My 1st
+    public List<List<Integer>> combinationSum3_old(int k, int n) {
         List<List<Integer>> result = new ArrayList<>();
         combine(result, new ArrayList<>(), k, n);
         return result;
