75ba4a5c1ac628e7488bad182b0df753211aafee
cdai <dc_726@163.com>
2016-08-15 15:29:36 -0400

    leetcode-080 remove duplicates from sorted array 2

--- 1-algorithm/13-leetcode/java/src/fundamentals/array/lc080_removeduplicatesfromsortedarray2/Solution.java
+++ 1-algorithm/13-leetcode/java/src/fundamentals/array/lc080_removeduplicatesfromsortedarray2/Solution.java
@@ -7,7 +7,56 @@ package fundamentals.array.lc080_removeduplicatesfromsortedarray2;
  * It doesn't matter what you leave beyond the new length.
  */
 public class Solution {
+
+    // Beautiful solution from stefan
+    // Idea: keep num if it's different from num[i-k]
     public int removeDuplicates(int[] nums) {
+        final int k = 2;
+        int i = 0;
+        for (int num : nums) {
+            if (i < k || nums[i - k] < num) {
+                nums[i++] = num;
+            }
+        }
+        return i;
+    }
+
+    // My 2nd: use counter to count duplicate times. O(N) time
+    public int removeDuplicates2(int[] nums) {
+        // Invariant: elements in [0,j) appear at most K times
+        final int k = 2;
+        int j = 1, count = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) {
+                if (++count <= k) {     // error: ++count, since nums[i] should be counted now
+                    nums[j++] = nums[i];
+                }
+            } else {
+                nums[j++] = nums[i];
+                count = 1;
+            }
+        }
+        return j;
+    }
+
+    // My 2nd: copy current and also next number if duplicate
+    // but it is hard to extend for K times situation
+    public int removeDuplicates3(int[] nums) {
+        // Invariant: elements in [0,j) appear at most twice.
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {     // error: i and j must start from 0. eg.[1,1]
+            if (i == 0 || nums[i] != nums[i - 1]) {
+                nums[j++] = nums[i];
+                if (i < nums.length - 1 && nums[i + 1] == nums[i]) {
+                    nums[j++] = nums[++i];
+                }
+            }
+        }
+        return j;
+    }
+
+    // My 1st
+    public int removeDuplicates1(int[] nums) {
         if (nums.length <= 2) {
             return nums.length;
         }
