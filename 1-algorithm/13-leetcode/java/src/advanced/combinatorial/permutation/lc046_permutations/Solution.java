8351987dc9fe0973567c5d758bf352cdab298e7f
cdai <dc_726@163.com>
2016-08-14 22:52:56 -0400

    leetcode-046 permutations

--- 1-algorithm/13-leetcode/java/src/advanced/combinatorial/permutation/lc046_permutations/Solution.java
+++ 1-algorithm/13-leetcode/java/src/advanced/combinatorial/permutation/lc046_permutations/Solution.java
@@ -2,7 +2,9 @@ package advanced.combinatorial.permutation.lc046_permutations;
 
 import java.util.ArrayList;
 import java.util.LinkedHashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 import java.util.Set;
 
 /**
@@ -19,7 +21,28 @@ import java.util.Set;
  */
 public class Solution {
 
+    // Iterative version: inspired by leetcode discuss
+    // Don't use iterator, which can avoid exception and tmp list
+    // O(N!) Beat 24%
     public List<List<Integer>> permute(int[] nums) {
+        Queue<List<Integer>> result = new LinkedList<>();
+        result.add(new ArrayList<>());
+        for (int num : nums) {
+            int size = result.size();
+            while (size-- > 0) {
+                List<Integer> per = result.poll();
+                for (int k = 0; k <= per.size(); k++) {
+                    List<Integer> newPer = new ArrayList<>(per);
+                    newPer.add(k, num);
+                    result.add(newPer);
+                }
+            }
+        }
+        return (List<List<Integer>>) result;
+    }
+
+    // My 1st: use Set to exclude picked candidates
+    public List<List<Integer>> permute1(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
         Set<Integer> path = new LinkedHashSet<>(); // TreeSet is sorted by element order; LinkedHashSet retains insertion order.
         permute(result, path, nums, 0);
