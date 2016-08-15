565b1f7c73461bcedf6a445062c38300a60bb099
cdai <dc_726@163.com>
2016-08-14 21:26:12 -0400

    leetcode-040 combination sum 2

--- 1-algorithm/13-leetcode/java/src/advanced/combinatorial/subset/lc040_combinationsum2/Solution.java
+++ 1-algorithm/13-leetcode/java/src/advanced/combinatorial/subset/lc040_combinationsum2/Solution.java
@@ -20,7 +20,40 @@ import java.util.Set;
  */
 public class Solution {
 
+    // My 2nd: O(N!) time, O(N) space
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(candidates);
+        doCombineSum(result, new ArrayList<>(), candidates, target, 0);
+        return result;
+    }
+
+    private void doCombineSum(List<List<Integer>> result,
+                              List<Integer> path, int[] candidates, int target, int k) {
+        if (target <= 0) {
+            if (target == 0) {
+                result.add(new ArrayList<>(path));
+            }
+            return;
+        }
+
+        for (int i = k; i < candidates.length; i++) {
+            if (i > k && candidates[i] == candidates[i - 1]) { // Very nice!!! Or use prev, both way is better than mine
+                continue;
+            }
+            path.add(candidates[i]);
+            doCombineSum(result, path, candidates, target - candidates[i], i + 1);
+            path.remove(path.size() - 1);
+
+            /*while (i < candidates.length - 1 && candidates[i] == candidates[i + 1]) { // bypass duplidate candidates
+                i++;
+            }*/
+            /* candidates[i] <> candidates[i + 1], then i++ in for move i to what we want */
+        }
+    }
+
+    // My 1st
+    public List<List<Integer>> combinationSum2_old(int[] candidates, int target) {
         // Avoid duplicates: List equals cannot handle like [2,1,5] and [1,2,5]
         Set<List<Integer>> result = new HashSet<>();
         Arrays.sort(candidates);
