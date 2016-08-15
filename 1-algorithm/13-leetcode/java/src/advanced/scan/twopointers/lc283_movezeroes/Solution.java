9176204f774792d3361e2e749fc3894bef7e1e4e
cdai <dc_726@163.com>
2016-08-15 19:05:09 -0400

    leetcode-283 move zeroes

--- 1-algorithm/13-leetcode/java/src/advanced/scan/twopointers/lc283_movezeroes/Solution.java
+++ 1-algorithm/13-leetcode/java/src/advanced/scan/twopointers/lc283_movezeroes/Solution.java
@@ -1,4 +1,6 @@
-package buildingblock.sorting.radix.lc283_movezeroes;
+package advanced.scan.twopointers.lc283_movezeroes;
+
+import java.util.Arrays;
 
 /**
  * Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
@@ -7,7 +9,20 @@ package buildingblock.sorting.radix.lc283_movezeroes;
  */
 public class Solution {
 
+    // My 2nd: O(N)
     public void moveZeroes(int[] nums) {
+        // Invariant: [0,j) has no zero
+        int j = 0;
+        for (int num : nums) {
+            if (num != 0) {
+                nums[j++] = num;
+            }
+        }
+        Arrays.fill(nums, j, nums.length, 0); // arg "to" is exclusive
+    }
+
+    // My 1st
+    public void moveZeroes1(int[] nums) {
         int i = 0;
         for (int j = 0; j < nums.length; j++) {
             if (nums[j] != 0) {
