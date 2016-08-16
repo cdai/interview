55e7f84117494e211a9e5ffac85a1a4657f2e4bd
cdai <dc_726@163.com>
2016-08-15 21:38:15 -0400

    leetcode-134 gas station

--- 1-algorithm/13-leetcode/java/src/advanced/scan/singlepointer/lc134_gasstation/Solution.java
+++ 1-algorithm/13-leetcode/java/src/advanced/scan/singlepointer/lc134_gasstation/Solution.java
@@ -9,7 +9,29 @@ package advanced.scan.singlepointer.lc134_gasstation;
  */
 public class Solution {
 
+    // My 2nd: Max cost, max gas, max gas-cost? All wrong...
+    // Improved by idea from leetcode discuss, update total from tank, beat 85%!
     public int canCompleteCircuit(int[] gas, int[] cost) {
+        if (gas.length != cost.length) {
+            return -1;
+        }
+
+        // Tank describes total gap - cost from [start,i]
+        // Tank < 0 means station between [start,i] is excluded
+        int total = 0, tank = 0, start = 0;
+        for (int i = 0; i < gas.length; i++) {
+            tank += (gas[i] - cost[i]);
+            if (tank < 0) {
+                start = i + 1;
+                total += tank;
+                tank = 0;
+            }
+        }
+        return total + tank >= 0 ? start : -1; // error: don't forget last tank
+    }
+    
+    // My 1st
+    public int canCompleteCircuit1(int[] gas, int[] cost) {
         if (gas.length == 0 || cost.length == 0 || gas.length != cost.length) {
             return -1;
         }
