8ea71db72b587920c62a96feb807b5da38de5492
cdai <dc_726@163.com>
2016-08-17 09:39:53 -0400

    leetcode-148 sort list

--- 1-algorithm/13-leetcode/java/src/buildingblock/sorting/merge/lc148_sortlist/Solution.java
+++ 1-algorithm/13-leetcode/java/src/buildingblock/sorting/merge/lc148_sortlist/Solution.java
@@ -7,8 +7,110 @@ import fundamentals.list.ListNode;
  */
 public class Solution {
 
-    // Simplify base case
+    public static void main(String[] args) {
+        ListNode head = new ListNode(2);
+        head.next = new ListNode(1);
+        head.next.next = new ListNode(3);
+        System.out.println(new Solution().sortList(head));
+    }
+
+    // What's people called: "Bottom-up merge sort"
     public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        // 1.Get total length of list
+        int length = 0;
+        for (ListNode n = head; n != null; n = n.next) {
+            length++;
+        }
+
+        // 2.Split and merge in increasing step (or block size whatever)
+        ListNode dummy = new ListNode(0);
+        for (int step = 1; step < length; step <<= 1) { // note: stop when step >= length, so merge(list/2, list/2) must complete!
+            ListNode prev = dummy;
+            ListNode cur = head;                        // error1: cur must start from head not dummy.next (null)
+            while (cur != null) {
+                ListNode half1 = cur;
+                cur = split(cur, step);
+                ListNode half2 = cur;
+                cur = split(cur, step);
+                prev = merge(prev, half1, half2);
+            }
+            head = dummy.next;
+        }
+        return dummy.next;
+    }
+
+    private ListNode split(ListNode node, int step) {
+        while (step-- > 1 && node != null) {
+            node = node.next;
+        }
+        if (node != null) {
+            ListNode nextHead = node.next;              // error2: don't forget cut list
+            node.next = null;
+            return nextHead;
+        }
+        return null;
+    }
+
+    // Merge and append to prev, return tail finally
+    private ListNode merge(ListNode prev, ListNode half1, ListNode half2) {
+        while (half1 != null || half2 != null) {
+            int val1 = (half1 != null) ? half1.val : Integer.MAX_VALUE;
+            int val2 = (half2 != null) ? half2.val : Integer.MAX_VALUE;
+            if (val1 < val2) {
+                prev.next = half1;
+                half1 = half1.next;
+            } else {
+                prev.next = half2;
+                half2 = half2.next;
+            }
+            prev = prev.next;
+        }
+        return prev;
+    }
+
+    // Clear but use O(logN) stack space
+    public ListNode sortList_recursion(ListNode head) {
+        if (head == null || head.next == null) { // Codes below can handle 1,2,...nodes, but this could improve performance
+            return head;
+        }
+
+        // 1.Find middle node and cut list into two halves
+        ListNode mid = head, fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            mid = mid.next;
+            fast = fast.next.next;
+        }
+        ListNode half1 = head, half2 = mid.next;
+        mid.next = null;
+
+        // 2.Sort two halves
+        half1 = sortList(half1);
+        half2 = sortList(half2);
+
+        // 3.Two-way merge
+        ListNode dummy = new ListNode(0);
+        ListNode prev = dummy;
+        while (half1 != null || half2 != null) {
+            int val1 = (half1 != null) ? half1.val : Integer.MAX_VALUE;
+            int val2 = (half2 != null) ? half2.val : Integer.MAX_VALUE;
+            if (val1 < val2) {
+                prev.next = half1;
+                half1 = half1.next;
+            } else {
+                prev.next = half2;
+                half2 = half2.next;
+            }
+            prev = prev.next;
+        }
+        return dummy.next;
+    }
+
+    // My 1st: Simplify base case
+    public ListNode sortList1(ListNode head) {
         // Only one node, just return
         if (head == null || head.next == null) {    // Optimze-0: simplify!!!
             return head;
