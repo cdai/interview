719b4bd035f1ab39455da2a2f60acf1a28b20eb0
cdai <dc_726@163.com>
2016-08-19 15:15:03 -0400

    leetcode-343 integer break

--- 1-algorithm/13-leetcode/java/src/miscellaneous/math/number/lc343_integerbreak/Solution.java
+++ 1-algorithm/13-leetcode/java/src/miscellaneous/math/number/lc343_integerbreak/Solution.java
@@ -12,7 +12,33 @@ public class Solution {
         System.out.println(new Solution().integerBreak(4));
     }
 
+    // My 2AC: more natural DP solution
     public int integerBreak(int n) {
+        // 1.Init except dp[n], since it cannot be itself and must break into two positive
+        int[] maxProd = new int[n + 1];
+        for (int i = 1; i < n; i++) {
+            maxProd[i] = i;
+        }
+
+        // 2.Compute max product
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j <= i - j; j++) { // Note: only check j <= i - j
+                maxProd[i] = Math.max(maxProd[i], maxProd[j] * maxProd[i - j]);
+            }
+        }
+        return maxProd[n];
+    }
+
+    // Stefan concise explanation:
+    // "If an optimal product contains a factor f >= 4,
+    // then you can replace it with factors 2 and f-2 without losing optimality,
+    // as 2*(f-2) = 2f-4 >= f. So you never need a factor greater than or equal to 4,
+    // meaning you only need factors 1, 2 and 3 (and 1 is of course wasteful and
+    // you'd only use it for n=2 and n=3, where it's needed).
+    // For the rest I agree, 3*3 is simply better than 2*2*2, so you'd never use 2 more than twice."
+
+    // My 1AC
+    public int integerBreak1(int n) {
         if (n == 2) {
             return 1;
         }
