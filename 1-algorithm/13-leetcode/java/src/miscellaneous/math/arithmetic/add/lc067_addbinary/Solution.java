ec01f860c91d095a92fc1c40b8646ed2be3af8f2
cdai <dc_726@163.com>
2016-08-18 22:05:34 -0400

    leetcode-067 add binary

--- 1-algorithm/13-leetcode/java/src/miscellaneous/math/arithmetic/add/lc067_addbinary/Solution.java
+++ 1-algorithm/13-leetcode/java/src/miscellaneous/math/arithmetic/add/lc067_addbinary/Solution.java
@@ -6,7 +6,23 @@ package miscellaneous.math.arithmetic.add.lc067_addbinary;
  */
 public class Solution {
 
+    // My 2AC: emulate elegant template code
     public String addBinary(String a, String b) {
+        StringBuilder result = new StringBuilder();
+        int i = a.length() - 1, j = b.length() - 1;
+        int carry = 0;
+        while (i >= 0 || j >= 0 || carry != 0) {
+            int b1 = (i >= 0) ? Character.getNumericValue(a.charAt(i--)) : 0;
+            int b2 = (j >= 0) ? Character.getNumericValue(b.charAt(j--)) : 0;
+            int sum = b1 + b2 + carry;
+            carry = sum / 2;
+            result.insert(0, sum % 2);
+        }
+        return result.length() == 0 ? "0" : result.toString();
+    }
+
+    // My 1AC: not bad
+    public String addBinary1(String a, String b) {
         if (a.length() == 0 || b.length() == 0) {
             return "0";
         }
