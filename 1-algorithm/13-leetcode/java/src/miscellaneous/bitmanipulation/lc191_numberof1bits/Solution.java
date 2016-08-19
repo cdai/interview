8aced5278b1215da1fab0604d1999858eb532893
cdai <dc_726@163.com>
2016-08-19 16:13:30 -0400

    leetcode-191 number of 1 bits

--- 1-algorithm/13-leetcode/java/src/miscellaneous/bitmanipulation/lc191_numberof1bits/Solution.java
+++ 1-algorithm/13-leetcode/java/src/miscellaneous/bitmanipulation/lc191_numberof1bits/Solution.java
@@ -9,13 +9,32 @@ package miscellaneous.bitmanipulation.lc191_numberof1bits;
 public class Solution {
 
     public static void main(String[] args) {
+        // Java shift operator test
+        // MIN_VALUE:   10000000000000000000000000000000
+        // >> 1:        11000000000000000000000000000000
+        // >>>1:        01000000000000000000000000000000
+        System.out.println(Integer.toBinaryString(Integer.MIN_VALUE));
+        System.out.println(Integer.toBinaryString(Integer.MIN_VALUE >> 1));
+        System.out.println(Integer.toBinaryString(Integer.MIN_VALUE >>> 1));
+
         System.out.println(new Solution().hammingWeight(11));
         System.out.println(new Solution().hammingWeight(Integer.MIN_VALUE));
         System.out.println(new Solution().hammingWeight((int) 2147483648L));
     }
 
-    // you need to treat n as an unsigned value
     public int hammingWeight(int n) {
+        int count = 0;
+        while (n != 0) {
+            if ((n & 1) == 1) {
+                count++;
+            }
+            n >>>= 1;
+        }
+        return count;
+    }
+
+    // you need to treat n as an unsigned value
+    public int hammingWeight1(int n) {
         int cnt = 0;
         for (int i = 0; i < 32; i++) {
             if (n == 0) {
