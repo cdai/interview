8433e18775bc44f5d575a1ab501b2686835bb242
cdai <dc_726@163.com>
2016-08-20 10:48:45 -0400

    leetcode-357 count numbers with unique digits

--- 1-algorithm/13-leetcode/java/src/miscellaneous/math/number/lc357_countnumberswithuniquedigits/Solution.java
+++ 1-algorithm/13-leetcode/java/src/miscellaneous/math/number/lc357_countnumberswithuniquedigits/Solution.java
@@ -7,7 +7,43 @@ package miscellaneous.math.number.lc357_countnumberswithuniquedigits;
  */
 public class Solution {
 
+    // My 2AC: backtracking solution. Note N -> N digits not N+1
+    public int countNumbersWithUniqueDigits2(int n) {
+        return doCount(n, new boolean[10], 0);
+    }
+
+    private int doCount(int n, boolean[] used, int d) {
+        if (d == n) {       // error-1: d starts from 0~N-1
+            return 1;
+        }
+        int total = 1;      // error-2: count internal nodes as well (root happens to be 0)
+        for (int i = (d == 0) ? 1 : 0; i <= 9; i++) {
+            if (!used[i]) {
+                used[i] = true;
+                total += doCount(n, used, d + 1);
+                used[i] = false;
+            }
+        }
+        return total;
+    }
+
+    // DP solution with counting technique
+    // total(n) = total(n-1) + f(K) (9 * 9 * 8 * ... (9 - k + 2))
     public int countNumbersWithUniqueDigits(int n) {
+        if (n == 0) return 1;
+
+        n = (n > 10) ? 10 : n; // Nice! Prevent overflow
+
+        int total = 1;
+        for (int i = 0, count = 1; i < n; i++) {
+            count *= (i == 0) ? 9 : 10 - i;
+            total += count;
+        }
+        return total;
+    }
+
+    // My 1AC: incremental thinking, but hard to read now...
+    public int countNumbersWithUniqueDigits1(int n) {
         if (n <= 0) {
             return 1;
         }
