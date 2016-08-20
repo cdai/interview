36ab619edd686fa1141481190d093205ce6a1e96
cdai <dc_726@163.com>
2016-08-20 11:04:09 -0400

    leetcode-172 factorial trailing zeroes

--- 1-algorithm/13-leetcode/java/src/miscellaneous/math/arithmetic/div/lc172_factorialtrailingzeroes/Solution.java
+++ 1-algorithm/13-leetcode/java/src/miscellaneous/math/arithmetic/div/lc172_factorialtrailingzeroes/Solution.java
@@ -6,11 +6,23 @@ package miscellaneous.math.arithmetic.div.lc172_factorialtrailingzeroes;
  */
 public class Solution {
 
+    // My 2AC
+    // Produce one zero:    5,10,15,20,25...
+    // Produce another zero:25(5*5),50(5*5*2)...
+    // So one factor 5 can produce 1 zero
+    public int trailingZeroes(int n) {
+        int count = 0;
+        for (long i = 5; i <= n; i *= 5) { // Be aware of overflow
+            count += n / i;
+        }
+        return count;
+    }
+
     // Eg. 136 / 5 = 27 (There're 27 '5' between 1..136, each one can cause one 0)
     // 136 / 25 = 5 (multiple of 25 can cause another 0, like 25,50,75,100,125, eg.25*4,75*8=600)
     // 136 / 125 = 1 (125 cause another 0, 3 in total, eg.125*8=1000)
     // So 136! has 33 trailing zero
-    public int trailingZeroes(int n) {
+    public int trailingZeroes1(int n) {
         if (n < 0) {
             return 0;
         }
