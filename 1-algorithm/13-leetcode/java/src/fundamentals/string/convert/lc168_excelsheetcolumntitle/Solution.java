2a6c1f599e8895c28d83ca125b40976cdfcd5f08
cdai <dc_726@163.com>
2016-08-19 21:33:11 -0400

    leetcode-168 excel sheet column title

--- 1-algorithm/13-leetcode/java/src/fundamentals/string/convert/lc168_excelsheetcolumntitle/Solution.java
+++ 1-algorithm/13-leetcode/java/src/fundamentals/string/convert/lc168_excelsheetcolumntitle/Solution.java
@@ -13,7 +13,30 @@ package fundamentals.string.convert.lc168_excelsheetcolumntitle;
  */
 public class Solution {
 
+    // Very nice recursive solution!
+    // It's suprisingly fast using recusion in this kind of small problem
     public String convertToTitle(int n) {
+        if (n-- <= 0) {
+            return "";
+        }
+        return convertToTitle(n / 26) + (char) ('A' + n % 26);
+    }
+
+    // My 2AC: negative, zero...
+    public String convertToTitle2(int n) {
+        if (n <= 0) {
+            return "";
+        }
+        StringBuilder result = new StringBuilder();
+        while (n-- > 0) {
+            result.insert(0, (char) ('A' + (n % 26)));
+            n /= 26;
+        }
+        return result.toString();
+    }
+
+    // My 1AC
+    public String convertToTitle1(int n) {
         StringBuilder title = new StringBuilder();
         while (n > 0) {
             n--;                                        // error1: since it's using 1~26, need sub 1 in each round
