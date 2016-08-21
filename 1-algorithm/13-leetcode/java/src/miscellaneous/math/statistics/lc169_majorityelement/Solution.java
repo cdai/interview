298e68f4ba8a34e2749a5c9b69fb4b785fcec7ee
cdai <dc_726@163.com>
2016-08-20 20:40:38 -0400

    leetcode-169 majority element

--- 1-algorithm/13-leetcode/java/src/miscellaneous/math/statistics/lc169_majorityelement/Solution.java
+++ 1-algorithm/13-leetcode/java/src/miscellaneous/math/statistics/lc169_majorityelement/Solution.java
@@ -7,10 +7,30 @@ package miscellaneous.math.statistics.lc169_majorityelement;
  */
 public class Solution {
 
+    // My 2AC: bitwise solution
+    public int majorityElement(int[] nums) {
+        int major = 0;
+        for (int i = 0; i < 32; i++) {
+            // 1.Get occurence of 1's at ith bit
+            int count = 0;
+            for (int num : nums) {
+                if ((num & (1 << i)) != 0) {
+                    count++;
+                }
+            }
+
+            // 2.If > n/2, majority must have it too
+            if (count * 2 > nums.length) {
+                major |= (1 << i);
+            }
+        }
+        return major;
+    }
+
     // 1.O(n),O(n): use hash to count each num occurence
     // 2.O(nlogn),O(logn): sort, then pick median as majority
     // 3.O(n),O(1): linear time majority vote algorithm
-    public int majorityElement(int[] nums) {
+    public int majorityElement1(int[] nums) {
         int candidate = 0, cnt = 0;
         for (int i = 0; i < nums.length; i++) {
             if (cnt == 0) {
