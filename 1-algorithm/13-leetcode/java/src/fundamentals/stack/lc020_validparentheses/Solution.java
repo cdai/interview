e2f7b21baafd9aa72213e1906b1f5dc883639430
cdai <dc_726@163.com>
2016-08-22 23:24:09 -0400

    leetcode-020 valid parentheses

--- 1-algorithm/13-leetcode/java/src/fundamentals/stack/lc020_validparentheses/Solution.java
+++ 1-algorithm/13-leetcode/java/src/fundamentals/stack/lc020_validparentheses/Solution.java
@@ -8,7 +8,61 @@ import java.util.Stack;
  */
 public class Solution {
 
+    // A Simple one!
     public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                } else if (c == ')' && stack.pop() != '(') {
+                    return false;
+                } else if (c == '}' && stack.pop() != '{') {
+                    return false;
+                } else if (c == ']' && stack.pop() != '[') {
+                    return false;
+                } /* true */
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    // My 2AC
+    public boolean isValid2(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            switch (c) {
+                case '(':
+                case '{':
+                case '[':
+                    stack.push(c);
+                    break;
+                case ')':
+                    if (stack.isEmpty() || stack.pop() != '(') {
+                        return false;
+                    }
+                    break;
+                case ']':
+                    if (stack.isEmpty() || stack.pop() != '[') {
+                        return false;
+                    }
+                    break;
+                case '}':
+                    if (stack.isEmpty() || stack.pop() != '{') { // error2: stack could be empty
+                        return false;
+                    }
+                    break;
+                default:
+                    return false;
+            }
+        }
+        return stack.isEmpty(); // error1: don't return true directly!
+    }
+
+    // My 1AC
+    public boolean isValid1(String s) {
         Stack<Character> stack = new Stack<>();
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
