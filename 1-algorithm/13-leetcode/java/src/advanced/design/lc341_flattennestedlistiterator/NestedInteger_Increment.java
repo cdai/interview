db2b1a7193a7652a07fd2483ec301f01eae9c898
cdai <dc_726@163.com>
2016-08-23 16:42:20 -0400

    leetcode-341 flatten nested list iterator

---
+++ 1-algorithm/13-leetcode/java/src/advanced/design/lc341_flattennestedlistiterator/NestedInteger_Increment.java
@@ -0,0 +1,40 @@
+package advanced.design.lc341_flattennestedlistiterator;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * Stefan incremental 'Real' iterator
+ */
+public class NestedInteger_Increment implements Iterator<Integer> {
+
+    private Stack<Iterator<NestedInteger>> stack;
+
+    private NestedInteger nextInt;
+
+    public NestedInteger_Increment(List<NestedInteger> nestedList) {
+        stack = new Stack<>();
+        stack.push(nestedList.iterator());
+    }
+
+    @Override
+    public Integer next() {
+        return nextInt.getInteger(); // assume user has called hasNext() every time, so stack top must be an Integer
+    }
+
+    @Override
+    public boolean hasNext() {
+        while (!stack.isEmpty()) {
+            if (!stack.peek().hasNext()) { // Top iterator is complete, oop it to start on next
+                stack.pop();
+            } else if ((nextInt = stack.peek().next()).isInteger()) { // Find an integer, now return
+                return true;
+            } else {
+                stack.push(nextInt.getList().iterator()); // Another list, push it and continue to find in it
+            }
+        }
+        return false;
+    }
+
+}
