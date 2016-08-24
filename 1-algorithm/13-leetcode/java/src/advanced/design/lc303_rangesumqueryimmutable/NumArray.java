f4ca4bafa1a05130fa0fc44ac6bbfd546058539b
cdai <dc_726@163.com>
2016-08-23 22:46:08 -0400

    leetcode-303 range sum query immutable

--- 1-algorithm/13-leetcode/java/src/advanced/design/lc303_rangesumqueryimmutable/NumArray.java
+++ 1-algorithm/13-leetcode/java/src/advanced/design/lc303_rangesumqueryimmutable/NumArray.java
@@ -33,4 +33,24 @@ public class NumArray {
         return sum[j] - (i > 0 ? sum[i - 1] : 0);
     }
 
+    // My 2AC
+    // sum[i] = sum of [0,i]
+    //private int[] sum;
+
+    public void NumArray2(int[] nums) {
+        sum = new int[nums.length];
+        if (nums.length > 0) {
+            sum[0] = nums[0];
+            for (int i = 1; i < nums.length; i++) {
+                sum[i] = sum[i - 1] + nums[i];
+            }
+        }
+    }
+
+    public int sumRange2(int i, int j) {
+        if (i < 0 || i > sum.length - 1 || j < 0 || j > sum.length - 1 || i > j) {
+            return 0;
+        }
+        return i == 0 ? sum[j] : sum[j] - sum[i - 1];
+    }
 }
