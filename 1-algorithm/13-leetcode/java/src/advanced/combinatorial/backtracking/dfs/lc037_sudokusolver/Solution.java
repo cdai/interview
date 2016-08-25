cc92033c0565346e5dbaa6dc7b244e357fe85986
cdai <dc_726@163.com>
2016-08-25 13:03:12 -0400

    leetcode-037 sudoku solver

--- 1-algorithm/13-leetcode/java/src/advanced/combinatorial/backtracking/dfs/lc037_sudokusolver/Solution.java
+++ 1-algorithm/13-leetcode/java/src/advanced/combinatorial/backtracking/dfs/lc037_sudokusolver/Solution.java
@@ -8,11 +8,59 @@ import java.util.Arrays;
  */
 public class Solution {
 
+    public static void main(String[] args) {
+        new Solution().solveSudoku(new char[][]
+                {"..9748...".toCharArray(),
+                "7........".toCharArray(),
+                ".2.1.9...".toCharArray(),
+                "..7...24.".toCharArray(),
+                ".64.1.59.".toCharArray(),
+                ".98...3..".toCharArray(),
+                "...8.3.2.".toCharArray(),
+                "........6".toCharArray(),
+                "...2759..".toCharArray()}
+        );
+    }
+
+    // Very straightforward version inspired by leetcode discuss
     public void solveSudoku(char[][] board) {
         doSolve(board, 0, 0);
     }
 
     private boolean doSolve(char[][] board, int row, int col) {
+        for (int i = row; i < 9; i++, col = 0) { // error: must reset col
+            for (int j = col; j < 9; j++) {
+                if (board[i][j] != '.') continue;
+                for (char num = '1'; num <= '9'; num++) {
+                    if (isValid(board, i, j, num)) {
+                        board[i][j] = num;
+                        if (doSolve(board, i, j + 1))
+                            return true;
+                        board[i][j] = '.';
+                    }
+                }
+                return false;               // Note terminate here
+            }
+        }
+        return true;
+    }
+
+    private boolean isValid(char[][] board, int row, int col, char num) {
+        int blkrow = (row / 3) * 3, blkcol = (col / 3) * 3; // Block no. is i/3, first element is i/3*3
+        for (int i = 0; i < 9; i++)
+            if (board[i][col] == num || board[row][i] == num ||
+                    board[blkrow + i / 3][blkcol + i % 3] == num)
+                return false;
+        return true;
+    }
+
+
+    // My 1AC
+    public void solveSudoku1(char[][] board) {
+        doSolve1(board, 0, 0);
+    }
+
+    private boolean doSolve1(char[][] board, int row, int col) {
         // Find next empty cell
         int[] empty = findNextEmptyCell(board, row, col);
         row = empty[0];
@@ -26,7 +74,7 @@ public class Solution {
         // Try each candidate
         for (char c : candidates(board, row, col)) {
             board[row][col] = c;
-            if (doSolve(board, row, col + 1)) {
+            if (doSolve1(board, row, col + 1)) {
                 return true;
             }
             board[row][col] = '.';
