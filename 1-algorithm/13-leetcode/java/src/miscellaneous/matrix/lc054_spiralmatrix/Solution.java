3a8a9f23e0a81946624876523cecc7a9eab741fa
cdai <dc_726@163.com>
2016-08-25 14:37:23 -0400

    leetcode-054 spiral matrix

--- 1-algorithm/13-leetcode/java/src/miscellaneous/matrix/lc054_spiralmatrix/Solution.java
+++ 1-algorithm/13-leetcode/java/src/miscellaneous/matrix/lc054_spiralmatrix/Solution.java
@@ -15,7 +15,42 @@ import java.util.List;
  */
 public class Solution {
 
+    public static void main(String[] args) {
+        System.out.println(new Solution().spiralOrder(new int[][]{{1, 2, 3, 4, 5}}));
+    }
+
+    // 2AC: Very straight forward solution from leetcode!
+    // When you finish a direction, remove that row/col by move rowbegin/end or colbegin/end
     public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> result = new ArrayList<>();
+        if (matrix.length == 0 || matrix[0].length == 0) return result;
+
+        int rowbegin = 0, rowend = matrix.length - 1;
+        int colbegin = 0, colend = matrix[0].length - 1;
+        while (rowbegin <= rowend && colbegin <= colend) {
+            // Move right and remove this row when finished
+            for (int i = colbegin; i <= colend; i++) result.add(matrix[rowbegin][i]);
+            rowbegin++;
+
+            // Move downwards and remove this column when finished
+            for (int i = rowbegin; i <= rowend; i++) result.add(matrix[i][colend]);
+            colend--;
+
+            if (rowbegin > rowend || colbegin > colend) break; // Without this If, no row will add col
+
+            // Move left and remove this row when finished
+            for (int i = colend; i >= colbegin; i--) result.add(matrix[rowend][i]);
+            rowend--;
+
+            // Move upwards and remove this column when finished
+            for (int i = rowend; i >= rowbegin; i--) result.add(matrix[i][colbegin]);
+            colbegin++;
+        }
+        return result;
+    }
+
+    // My 1AC
+    public List<Integer> spiralOrder1(int[][] matrix) {
         List<Integer> result = new ArrayList<>();
         if (matrix.length == 0 || matrix[0].length == 0) {
             return result;
