22cb71bc9e67bb46e07a45d8fd2603aba6a1e868
cdai <dc_726@163.com>
2016-08-26 17:50:29 -0400

    leetcode-212 word search 2

---
+++ 1-algorithm/13-leetcode/java/src/advanced/datastructure/trie/lc212_wordsearch2/Solution.java
@@ -0,0 +1,73 @@
+package advanced.datastructure.trie.lc212_wordsearch2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Given a 2D board and a list of words from the dictionary, find all words in the board.
+ * Each word must be constructed from letters of sequentially adjacent cell,
+ * where "adjacent" cells are those horizontally or vertically neighboring.
+ * The same letter cell may not be used more than once in a word.
+ * For example, Given words = ["oath","pea","eat","rain"] and board =
+ * [
+ *  ['o','a','a','n'],
+ *  ['e','t','a','e'],
+ *  ['i','h','k','r'],
+ *  ['i','f','l','v']
+ * ]
+ * Return ["eat","oath"].
+ * Note: You may assume that all inputs are consist of lowercase letters a-z.
+ */
+public class Solution {
+
+    public List<String> findWords(char[][] board, String[] words) {
+        List<String> result = new ArrayList<>();
+        TrieNode trie = TrieNode.buildTree(words);
+        for (int i = 0; i < board.length; i++)
+            for (int j = 0; j < board[i].length; j++)
+                doFindWords(result, trie, board, i, j);
+        return result;
+    }
+
+    private void doFindWords(List<String> result, TrieNode p, char[][] board, int row, int col) {
+        char c = board[row][col];
+        if (!Character.isLetter(board[row][col]) || p.next[c - 'a'] == null)
+            return;
+
+        p = p.next[c - 'a'];                // Optimize-1: don't start from root every time
+        if (p.word != null) {
+            result.add(p.word);
+            p.word = null;                  // Optimize-2: de-duplicate without Set
+        }
+
+        board[row][col] = '#';              // Optimize-3: terminate earlier
+        if (row > 0) doFindWords(result, p, board, row - 1, col);
+        if (col > 0) doFindWords(result, p, board, row, col - 1);
+        if (row < board.length - 1) doFindWords(result, p, board, row + 1, col);
+        if (col < board[0].length - 1) doFindWords(result, p, board, row, col + 1);
+        board[row][col] = c;
+    }
+
+    static class TrieNode {                 // Optimize-4: don't store char
+        String word;
+        TrieNode[] next;
+        TrieNode() { this.next = new TrieNode[26]; }
+
+        static TrieNode buildTree(String[] words) {
+            TrieNode root = new TrieNode();
+            for (String word : words) {
+                TrieNode cur = root;
+                for (char c : word.toCharArray()) {
+                    int idx = c - 'a';
+                    if (cur.next[idx] == null) {
+                        cur.next[idx] = new TrieNode();
+                    }
+                    cur = cur.next[idx];
+                }
+                cur.word = word;
+            }
+            return root;
+        }
+    }
+
+}
