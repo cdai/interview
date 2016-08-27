f938895ba2556d737accf4f98af81627e47a7e5f
cdai <dc_726@163.com>
2016-08-26 22:33:46 -0400

    leetcode-050 pow

--- 1-algorithm/13-leetcode/java/src/buildingblock/divideandconquer/lc050_pow/Solution.java
+++ 1-algorithm/13-leetcode/java/src/buildingblock/divideandconquer/lc050_pow/Solution.java
@@ -4,7 +4,47 @@ package buildingblock.divideandconquer.lc050_pow;
  * Implement pow(x, n).
  */
 public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().myPow(2, 5));
+    }
+
+    // Very nice iterative solution
     public double myPow(double x, int n) {
+        double ret = 1, pow = x;
+        long absn = Math.abs((long) n);
+        while (absn > 0) {      // n != 0 doesn't work due to sign
+            if ((absn & 1) == 1)// avoid sign affect. don't use %
+                ret *= pow;     // pow not x. eg.7->3->1. ret=x,pow=x^2. ret=x^3,pow=x^4.
+            pow *= pow;
+            absn >>= 1;
+        }
+        return n > 0 ? ret : 1 / ret;
+    }
+
+    public double myPow_iterative(double x, int n) {
+        double ret = 1;
+        for (long absn = Math.abs((long) n), i; absn > 0; absn -= i) {
+            double pow = x;
+            for (i = 1; i < absn / 2; i <<= 1)
+                pow *= pow;
+            ret *= pow;
+        }
+        return n > 0 ? ret : 1 / ret; // Would be any different for 1/ret here or 1/x each step?
+    }
+
+    // Overflow risk if we Math.abs(n). O(logN) time.
+    public double myPow_recursive(double x, int n) {
+        if (n == 0) return 1;
+
+        double pow = myPow(x, n / 2);
+        if (n % 2 == 0)
+            return pow * pow;
+        return (n < 0) ? pow * pow / x : pow * pow * x;
+    }
+
+    // My 1AC
+    public double myPow1(double x, int n) {
         if (n == 0) {
             return 1;
         } else if (n == -1) {
@@ -13,7 +53,7 @@ public class Solution {
             return x;
         }
 
-        double r = myPow(x, n / 2);
+        double r = myPow1(x, n / 2);
         r = r * r;
 
         if (n % 2 != 0) {
