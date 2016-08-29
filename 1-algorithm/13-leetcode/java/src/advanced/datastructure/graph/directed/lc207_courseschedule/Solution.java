81d23dc6dca5ec5fa70225c9df4d5225e36c6372
cdai <dc_726@163.com>
2016-08-29 19:32:46 -0400

    leetcode-207 course schedule

---
+++ 1-algorithm/13-leetcode/java/src/advanced/datastructure/graph/directed/lc207_courseschedule/Solution.java
@@ -0,0 +1,69 @@
+package advanced.datastructure.graph.directed.lc207_courseschedule;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ */
+public class Solution {
+
+    // Topological sort
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        // Adjacent matrix is convenient for exist check
+        int[] indegree = new int[numCourses];
+        boolean[][] adj = new boolean[numCourses][numCourses];
+        for (int[] e : prerequisites) {
+            int cur = e[1], pre = e[0];
+            if (!adj[cur][pre])     // error: input could contains duplicate edge, why...
+                indegree[pre]++;
+            adj[cur][pre] = true;
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int v = 0; v < numCourses; v++)
+            if (indegree[v] == 0)
+                queue.offer(v);
+
+        int count = 0;
+        for (; !queue.isEmpty(); count++) {
+            int cur = queue.poll();
+            for (int pre = 0; pre < numCourses; pre++)
+                if (adj[cur][pre] && --indegree[pre] == 0)
+                    queue.offer(pre);
+        }
+        return count == numCourses;
+    }
+
+    // TLE since DFS does many redudent work. Eg.2->1->0, it checks 2 then 1 then 0...
+    public boolean canFinish_dfs(int numCourses, int[][] prerequisites) {
+        // 1.Create adjacent list
+        List<Integer>[] adj = new List[numCourses];
+        for (int[] pre : prerequisites) {
+            if (adj[pre[1]] == null)        // Save memory since only some have prerequisite
+                adj[pre[1]] = new ArrayList<>();
+            adj[pre[1]].add(pre[0]);        // From cur -> prerequisite
+        }
+
+        // 2.DFS check each "island" if cycle exist
+        boolean[] visited = new boolean[numCourses];
+        for (int v = 0; v < numCourses; v++)
+            if (ifCycleExist(v, adj, visited))
+                return false;
+        return true;
+    }
+
+    private boolean ifCycleExist(int vertex, List<Integer>[] adj, boolean[] visited) {
+        if (visited[vertex]) return true;
+        if (adj[vertex] == null) return false;
+
+        visited[vertex] = true;
+        for (int neighbor : adj[vertex])
+            if (ifCycleExist(neighbor, adj, visited))
+                return true;
+        visited[vertex] = false; // Here is the key point! No cycle at this or lower level
+        return false;
+    }
+
+}
