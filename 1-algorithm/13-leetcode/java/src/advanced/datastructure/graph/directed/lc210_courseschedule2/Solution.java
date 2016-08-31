a0b81275f266476709d058121acc71df31ec22c4
cdai <dc_726@163.com>
2016-08-30 22:16:30 -0400

    leetcode-210 course schedule 2

--- 1-algorithm/13-leetcode/java/src/advanced/datastructure/graph/directed/lc210_courseschedule2/Solution.java
+++ 1-algorithm/13-leetcode/java/src/advanced/datastructure/graph/directed/lc210_courseschedule2/Solution.java
@@ -6,6 +6,19 @@ import java.util.List;
 import java.util.Queue;
 
 /**
+ * There are a total of n courses you have to take, labeled from 0 to n - 1.
+ * Some courses may have prerequisites, for example to take course 0 you have to first take course 1,
+ * which is expressed as a pair: [0,1]. Given the total number of courses and a list of prerequisite pairs,
+ * return the ordering of courses you should take to finish all courses.
+ * There may be multiple correct orders, you just need to return one of them.
+ * If it is impossible to finish all courses, return an empty array.
+ * For example:
+ *  2, [[1,0]]. There are a total of 2 courses to take. To take course 1 you should have finished course 0.
+ *      So the correct course order is [0,1]
+ *  4, [[1,0],[2,0],[3,1],[3,2]]. There are a total of 4 courses to take.
+ *      To take course 3 you should have finished both courses 1 and 2.
+ *      Both courses 1 and 2 should be taken after you finished course 0.
+ *      So one correct course order is [0,1,2,3]. Another correct ordering is[0,2,1,3].
  */
 public class Solution {
 
@@ -20,22 +33,23 @@ public class Solution {
         int[] indegree = new int[numCourses];
         for (int[] e : prerequisites) {
             int cur = e[0], pre = e[1];
-            indegree[pre]++;
-            adj[cur].add(pre);
+            indegree[cur]++;
+            adj[pre].add(cur);      // pre -> cur will get the right order
         }
 
         Queue<Integer> queue = new LinkedList<>();
-        for (int v : indegree)
-            queue.offer(v);
+        for (int i = 0; i < numCourses; i++)
+            if (indegree[i] == 0)
+                queue.offer(i);
 
         int[] order = new int[numCourses];
         int visited = 0;
         while (!queue.isEmpty()) {
-            int cur = queue.poll();
-            order[visited++] = cur;
-            for (int pre : adj[cur])
-                if (--indegree[pre] == 0)
-                    queue.offer(pre);
+            int pre = queue.poll();
+            order[visited++] = pre;
+            for (int cur : adj[pre])
+                if (--indegree[cur] == 0)
+                    queue.offer(cur);
         }
         return visited == numCourses ? order : new int[0];
     }
