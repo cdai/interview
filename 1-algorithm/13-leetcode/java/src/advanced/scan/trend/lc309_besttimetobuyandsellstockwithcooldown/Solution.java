5ed9394ee4250775fc879926392be17b4a2246f4
cdai <dc_726@163.com>
2016-09-04 16:59:55 -0400

    leetcode-309 best time to buy and sell stock with cooldown

--- 1-algorithm/13-leetcode/java/src/advanced/scan/trend/lc309_besttimetobuyandsellstockwithcooldown/Solution.java
+++ 1-algorithm/13-leetcode/java/src/advanced/scan/trend/lc309_besttimetobuyandsellstockwithcooldown/Solution.java
@@ -14,7 +14,40 @@ public class Solution {
         System.out.println(new Solution().maxProfit(new int[]{1, 2, 3, 10, 2, 4}));
     }
 
+    // Replace N space with variable because of limited states
+    // dependency: sell[i-1] => buy[i-1] => rest[i-1] => sell[i-1] (use prev break cycle)
     public int maxProfit(int[] prices) {
+        if (prices.length == 0) return 0;
+        int rest = 0, buy = -prices[0], sell = Integer.MIN_VALUE;
+        for (int i = 1; i < prices.length; i++) {
+            int prev = sell;
+            sell = buy + prices[i];
+            buy = Math.max(rest - prices[i], buy);
+            rest = Math.max(rest, prev);
+        }
+        return Math.max(rest, sell);
+    }
+
+    // Brilliant understanding from state machine view
+    public int maxProfit_statemachine(int[] prices) {
+        if (prices.length == 0) return 0;
+        int n = prices.length;
+        int[] rest = new int[n];
+        int[] buy = new int[n];
+        int[] sell = new int[n];
+
+        rest[0] = 0;
+        buy[0] = -prices[0];
+        sell[0] = Integer.MIN_VALUE;
+        for (int i = 1; i < n; i++) {
+            rest[i] = Math.max(rest[i - 1], sell[i - 1]);
+            buy[i] = Math.max(rest[i - 1] - prices[i], buy[i - 1]);
+            sell[i] = buy[i - 1] + prices[i];
+        }
+        return Math.max(rest[n - 1], sell[n - 1]); // Max profit is impossible be at Buy state
+    }
+
+    public int maxProfit1(int[] prices) {
         // Max profit before i ending with buy or sell
         int[] profitEndWithBuy = new int[prices.length + 2];
         int[] profitEndWithSell = new int[prices.length + 2];
