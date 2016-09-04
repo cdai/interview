be410a34cc32852b897bb657ccf80d36149b7883
cdai <dc_726@163.com>
2016-09-03 21:50:37 -0400

    leetcode-214 shortest palindrome

--- 1-algorithm/13-leetcode/java/src/advanced/dp/palindrome/lc214_shortestpalindrome/Solution.java
+++ 1-algorithm/13-leetcode/java/src/advanced/dp/palindrome/lc214_shortestpalindrome/Solution.java
@@ -7,8 +7,53 @@ package advanced.dp.palindrome.lc214_shortestpalindrome;
  */
 public class Solution {
 
-    // Timeout for "aaaaaa..."
+    public static void main(String[] args) {
+        System.out.println(new Solution().shortestPalindrome("aacecaaa"));
+    }
+
+    // What the hell...
+    public String shortestPalindrome3(String s) {
+        int j = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == s.charAt(j)) { j += 1; }
+        }
+        if (j == s.length()) { return s; }
+        String suffix = s.substring(j);
+        return new StringBuffer(suffix).reverse().toString() + shortestPalindrome(s.substring(0, j)) + suffix;
+    }
+
+    // A little better using extend approach. Still O(N^2).
     public String shortestPalindrome(String s) {
+        int i = s.length() - 1;
+        for (; i > 0 && !isPalindrome(s, i); i--);
+        return new StringBuilder(s.substring(i + 1)).reverse().append(s).toString();
+    }
+
+    private boolean isPalindrome(String s, int end) {
+        int mid1 = end / 2, mid2 = (end % 2 == 0) ? mid1 : mid1 + 1;
+        for (int i = mid1, j = mid2; i >= 0 && j <= end; i--, j++)
+            if (s.charAt(i) != s.charAt(j))
+                return false;
+        return true;
+    }
+
+    // O(N^2)
+    public String shortestPalindrome_bruteforce(String s) {
+        // Find center with longest palindrome
+        int i = s.length() - 1;
+        for (; i > 0 && !isPalindrome(s, i); i--);
+        return new StringBuilder(s.substring(i + 1)).reverse().append(s).toString();
+    }
+
+    private boolean isPalindrome2(String s, int i) {
+        for (int j = 0; j < i; j++, i--)
+            if (s.charAt(j) != s.charAt(i))
+                return false;
+        return true;
+    }
+
+    // Timeout for "aaaaaa..."
+    public String shortestPalindrome1(String s) {
         if (s.length() < 2) {
             return s;
         }
