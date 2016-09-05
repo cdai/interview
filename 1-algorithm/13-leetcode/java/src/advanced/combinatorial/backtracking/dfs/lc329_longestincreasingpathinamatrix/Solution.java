f80e2af4815309f3e9126934ec43cde187d1c3a9
cdai <dc_726@163.com>
2016-09-05 11:11:44 -0400

    leetcode-329 longest increasing path in a matrix

---
+++ 1-algorithm/13-leetcode/java/src/advanced/combinatorial/backtracking/dfs/lc329_longestincreasingpathinamatrix/Solution.java
@@ -0,0 +1,78 @@
+package advanced.combinatorial.backtracking.dfs.lc329_longestincreasingpathinamatrix;
+
+/**
+ * Given an integer matrix, find the length of the longest increasing path.
+ * From each cell, you can either move to four directions: left, right, up or down.
+ * You may NOT move diagonally or move outside of the boundary (i.e. wrap-around is not allowed).
+ * Example 1:
+ *  nums = [
+ *  [9,9,4],
+ *  [6,6,8],
+ *  [2,1,1]
+ *  ]
+ * Return 4. The longest increasing path is [1, 2, 6, 9].
+ * Example 2:
+ *  nums = [
+ *  [3,4,5],
+ *  [3,2,6],
+ *  [2,2,1]
+ *  ]
+ * Return 4. The longest increasing path is [3, 4, 5, 6]. Moving diagonally is not allowed.
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().longestIncreasingPath(
+                new int[][]{{9, 9, 4}, {6, 10, 8}, {2, 1, 1}}));
+    }
+
+    private int[][] memo;
+
+    private int[][] dirs = { {-1, 0}, {1, 0}, {0, -1}, {0, 1} };
+
+    // My 2AC: due to memo, every cell is computed once, so O(MN) time
+    // No need to mark or save visited cells, since sequence must be increasing
+    public int longestIncreasingPath(int[][] A) {
+        if (A.length == 0 || A[0].length == 0) return 0;
+        this.memo = new int[A.length][A[0].length];
+        int max = 0;
+        for (int i = 0; i < A.length; i++)
+            for (int j = 0; j < A[i].length; j++)
+                max = Math.max(max, dfs(A, i, j, Integer.MIN_VALUE));
+        return max;
+    }
+
+    private int dfs(int[][] A, int i, int j, int prev) {
+        if (i < 0 || i >= A.length || j < 0 || j >= A[0].length) return 0;
+        if (A[i][j] <= prev) return 0;
+        if (memo[i][j] > 0) return memo[i][j];
+
+        int max = 1;
+        for (int[] dir : dirs)
+            max = Math.max(max, dfs(A, i + dir[0], j + dir[1], A[i][j]) + 1);
+        memo[i][j] = max;
+        return max;
+    }
+
+    // Totally wrong! Since path could move from 4 directions, not only left and top.
+    public int longestIncreasingPath_dp(int[][] A) {
+        if (A.length == 0 || A[0].length == 0) return 0;
+
+        int[] dp = new int[A[0].length];
+        dp[0] = 1;
+        for (int i = 1; i < A[0].length; i++)
+            dp[i] = (A[0][i] > A[0][i - 1]) ? dp[i - 1] + 1 : 1;
+
+        int max = 0;
+        for (int i = 1; i < A.length; i++) {
+            dp[0] = (A[i][0] > A[i - 1][0]) ? dp[0] + 1 : 1;
+            for (int j = 1; j < A[i].length; j++) {
+                dp[j] = Math.max((A[i][j] > A[i][j - 1]) ? dp[j - 1] + 1 : 1,
+                        (A[i][j] > A[i - 1][j]) ? dp[j] + 1 : 1);
+                max = Math.max(max, dp[j]);
+            }
+        }
+        return max;
+    }
+
+}
--- 1-algorithm/13-leetcode/java/src/advanced/dp/statemac/lc188_besttimetobuyandsellstock4/Solution.java
+++ 1-algorithm/13-leetcode/java/src/advanced/dp/statemac/lc188_besttimetobuyandsellstock4/Solution.java
@@ -1,9 +1,31 @@
 package advanced.dp.statemac.lc188_besttimetobuyandsellstock4;
 
 /**
+ *
  */
 public class Solution {
 
+    // My 2AC: O(N^2)
+    public int maxProfit(int k, int[] prices) {
+        if (prices.length < 2) return 0;
+        int n = prices.length;
+        if (k >= n / 2) { // if k >= n/2, then you can make maximum number of transactions
+            int pro = 0;
+            for (int i = 1; i < n; i++)
+                if (prices[i] > prices[i - 1])
+                    pro += prices[i] - prices[i - 1];
+            return pro;
+        }
 
+        int[][] dp = new int[k + 1][n]; // sell states of K transactions
+        for (int i = 1; i <= k; i++) {
+            int buy = -prices[0];       // buy state
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = Math.max(dp[i][j - 1], buy + prices[j]);
+                buy = Math.max(buy, dp[i - 1][j] - prices[j]);
+            }
+        }
+        return dp[k][n - 1];
+    }
 
 }
