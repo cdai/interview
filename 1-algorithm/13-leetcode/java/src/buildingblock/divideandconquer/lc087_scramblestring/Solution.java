5f8d0afb7958130a32cbd33091b7dcd5828f3bf2
cdai <dc_726@163.com>
2016-09-08 15:31:49 -0400

    leetcode-087 scramble string

--- 1-algorithm/13-leetcode/java/src/buildingblock/divideandconquer/lc087_scramblestring/Solution.java
+++ 1-algorithm/13-leetcode/java/src/buildingblock/divideandconquer/lc087_scramblestring/Solution.java
@@ -60,6 +60,28 @@ public class Solution {
         return false;
     }
 
+    /*
+    dp[i][j][l] means whether s2.substr(j,l) is a scrambled string of s1.substr(i,l) or not.
+class Solution {
+public:
+    bool isScramble(string s1, string s2) {
+        int len=s1.size();
+        bool dp[100][100][100]={false};
+        for (int i=len-1;i>=0;i--)
+            for (int j=len-1;j>=0;j--) {
+                dp[i][j][1]=(s1[i]==s2[j]);
+                for (int l=2;i+l<=len && j+l<=len;l++) {
+                    for (int n=1;n<l;n++) {
+                        dp[i][j][l]|=dp[i][j][n]&&dp[i+n][j+n][l-n];
+                        dp[i][j][l]|=dp[i][j+l-n][n]&&dp[i+n][j][l-n];
+                    }
+                }
+            }
+        return dp[0][0][len];
+    }
+};
+     */
+
     // My 1st attempt which is wrong...
     public boolean isScramble1(String s1, String s2) {
         System.out.println(s1 + " - " + s2);
