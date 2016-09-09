fe275c9f9ffdcaca9be1e22dde9089f8b8156640
cdai <dc_726@163.com>
2016-09-09 09:18:19 -0400

    leetcode-221 maximal square

--- 1-algorithm/13-leetcode/java/src/advanced/dp/twodim/lc221_maximalsquare/Solution.java
+++ 1-algorithm/13-leetcode/java/src/advanced/dp/twodim/lc221_maximalsquare/Solution.java
@@ -22,7 +22,26 @@ public class Solution {
                 }));
     }
 
+    // O(N^2) time. dp[i][j] denotes the max length of a square of 1's whose bottom-right is at i-1,j-1
+    // Thus we save the trouble of boundary initialization.
     public int maximalSquare(char[][] matrix) {
+        if (matrix.length == 0 || matrix[0].length == 0) return 0;
+
+        int[][] dp = new int[matrix.length + 1][matrix[0].length + 1];
+        int max = 0;
+        for (int i = 1; i <= matrix.length; i++) {
+            for (int j = 1; j <= matrix[i - 1].length; j++) {
+                if (matrix[i - 1][j - 1] == '1') {
+                    dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
+                    max = Math.max(max, dp[i][j]);
+                }
+            }
+        }
+        return max * max;
+    }
+
+    // My 1AC: No need to init boundary
+    public int maximalSquare1(char[][] matrix) {
         if (matrix.length == 0 || matrix[0].length == 0) { // error2: error if extract m and n before this guard
             return 0;
         }
