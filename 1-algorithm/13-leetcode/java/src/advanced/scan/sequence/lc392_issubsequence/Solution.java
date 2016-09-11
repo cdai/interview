06456f474d021c70f7e407607730d1a0acdc0e1c
cdai <dc_726@163.com>
2016-09-11 10:22:14 -0400

    leetcode-392 is subsequence

--- 1-algorithm/13-leetcode/java/src/advanced/scan/sequence/lc392_issubsequence/Solution.java
+++ 1-algorithm/13-leetcode/java/src/advanced/scan/sequence/lc392_issubsequence/Solution.java
@@ -1,5 +1,9 @@
 package advanced.scan.sequence.lc392_issubsequence;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Given a string s and a string t, check if s is subsequence of t.
  * You may assume that there is only lower case English letters in both s and t. t is potentially a very long (length ~= 500,000) string,
@@ -13,8 +17,38 @@ package advanced.scan.sequence.lc392_issubsequence;
  */
 public class Solution {
 
-    // My 2AC: O(M+N)
+    // Follow-up: O(N) time for pre-processing, O(Mlog?) for each S.
+    // Eg-1. s="abc", t="bahbgdca"
+    // idx=[a={1,7}, b={0,3}, c={6}]
+    //  i=0 ('a'): prev=1
+    //  i=1 ('b'): prev=3
+    //  i=2 ('c'): prev=6 (return true)
+    // Eg-2. s="abc", t="bahgdcb"
+    // idx=[a={1}, b={0,6}, c={5}]
+    //  i=0 ('a'): prev=1
+    //  i=1 ('b'): prev=6
+    //  i=2 ('c'): prev=? (return false)
     public boolean isSubsequence(String s, String t) {
+        List<Integer>[] idx = new List[256]; // Just for clarity
+        for (int i = 0; i < t.length(); i++) {
+            if (idx[t.charAt(i)] == null)
+                idx[t.charAt(i)] = new ArrayList<>();
+            idx[t.charAt(i)].add(i);
+        }
+
+        int prev = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (idx[s.charAt(i)] == null) return false; // Note: char of S does NOT exist in T causing NPE
+            int j = Collections.binarySearch(idx[s.charAt(i)], prev);
+            if (j < 0) j = -j - 1;
+            if (j == idx[s.charAt(i)].size()) return false;
+            prev = idx[s.charAt(i)].get(j) + 1;
+        }
+        return true;
+    }
+
+    // My 2AC: O(M+N)
+    public boolean isSubsequence_once(String s, String t) {
         int i = 0;
         for (int j = 0; i < s.length() && j < t.length(); j++)
             if (s.charAt(i) == t.charAt(j)) i++;
