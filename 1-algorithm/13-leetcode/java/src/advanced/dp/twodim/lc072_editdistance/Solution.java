9c9e15b7562cded36ecdfcd322cefab0519a2490
cdai <dc_726@163.com>
2016-09-14 17:34:13 -0400

    leetcode-072 edit distance

--- 1-algorithm/13-leetcode/java/src/advanced/dp/twodim/lc072_editdistance/Solution.java
+++ 1-algorithm/13-leetcode/java/src/advanced/dp/twodim/lc072_editdistance/Solution.java
@@ -14,7 +14,26 @@ public class Solution {
         System.out.println(new Solution().minDistance("apple", "are"));
     }
 
+    // O(N^2) time.
+    // Prefix approach: dp[i][j] = w1[1:i] and w2[1:j] min dist
+    // Link with smaller problem:
+    //      1.delete w1[i] => dp[i-1][j] + 1
+    //      2.delete w2[j] => dp[i][j-1] + 1
+    //      3.substitute i to j if diff: dp[i-1][j-1] + 1
     public int minDistance(String word1, String word2) {
+        int[][] dp = new int[word1.length() + 1][word2.length() + 1];
+        for (int i = 1; i <= word1.length(); i++)
+            dp[i][0] = i;
+        for (int j = 1; j <= word2.length(); j++)
+            dp[0][j] = j;
+        for (int i = 1; i <= word1.length(); i++)
+            for (int j = 1; j <= word2.length(); j++)
+                dp[i][j] = Math.min(dp[i - 1][j - 1] + (word1.charAt(i - 1) == word2.charAt(j - 1) ? 0 : 1),
+                        Math.min(dp[i - 1][j] + 1, dp[i][j - 1] + 1));
+        return dp[word1.length()][word2.length()];
+    }
+
+    public int minDistance1(String word1, String word2) {
         if (word1 == null || word2 == null) {
             return 0;
         }
