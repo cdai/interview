fa8f83714d363c879cc8f1e39075a9109c99d244
cdai <dc_726@163.com>
2016-09-25 22:42:07 -0400

    My 3AC

--- 1-algorithm/13-leetcode/java/src/fundamentals/string/search/lc161_oneeditdistance/Solution.java
+++ 1-algorithm/13-leetcode/java/src/fundamentals/string/search/lc161_oneeditdistance/Solution.java
@@ -16,8 +16,22 @@ public class Solution {
         System.out.println(!s.isOneEditDistance("aXcXef", "abcdef"));
     }
 
-    // My solution. O(N) time.
+    // My 3AC. O(N) time using one loop.
     public boolean isOneEditDistance(String s, String t) {
+        if (Math.abs(s.length() - t.length()) > 1) return false;
+        for (int i = 0; i < Math.min(s.length(), t.length()); i++) {
+            if (s.charAt(i) == t.charAt(i)) continue;
+            if (s.length() == t.length())
+                return s.substring(i + 1).equals(t.substring(i + 1));
+            if (s.length() > t.length())
+                return s.substring(i + 1).equals(t.substring(i));
+            return s.substring(i).equals(t.substring(i + 1));
+        }
+        return true; /* Only last char different. eg."abcd" "abc" */
+    }
+
+    // My solution. O(N) time.
+    public boolean isOneEditDistance2(String s, String t) {
         if (s.length() == t.length()) {
             for (int i = 0, diff = 0; i < s.length(); i++)
                 if (s.charAt(i) != t.charAt(i) && ++diff > 1) return false;
