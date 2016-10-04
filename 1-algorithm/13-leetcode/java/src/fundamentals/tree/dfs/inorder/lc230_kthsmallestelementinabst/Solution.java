642ab29efc97d02ec673fc359c6d40ee1246b5e0
cdai <dc_726@163.com>
2016-10-04 12:01:15 -0400

    3rd AC

--- 1-algorithm/13-leetcode/java/src/fundamentals/tree/dfs/inorder/lc230_kthsmallestelementinabst/Solution.java
+++ 1-algorithm/13-leetcode/java/src/fundamentals/tree/dfs/inorder/lc230_kthsmallestelementinabst/Solution.java
@@ -19,15 +19,47 @@ public class Solution {
     public static void main(String[] args) {
         TreeNode root = new TreeNode(9);
         root.left = new TreeNode(3);
-        root.right = new TreeNode(2);
-        root.left.left = new TreeNode(4);
-        root.left.right = new TreeNode(1);
-        root.right.right = new TreeNode(6);
-        System.out.println(new Solution().kthSmallest(root, 7));
+        root.right = new TreeNode(12);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(4);
+        root.right.right = new TreeNode(15);
+        System.out.println(new Solution().kthSmallest(root, 4));
+
+        System.out.println(new Solution().secondLargest(root));
+    }
+
+    // O(logN) time. Second largest node is largest node's parent or rightmost of its left subtree
+    public int secondLargest(TreeNode root) {
+        TreeNode p = root, prev = null;
+        for (; p.right != null; p = p.right) prev = p;
+
+        if (prev == null) return 0; // Only one node in tree
+        if (p.left == null) return prev.val;
+
+        p = p.left;
+        for (; p.right != null; p = p.right);
+        return p.val;
+    }
+
+    // My 3AC
+    public int kthSmallest(TreeNode root, int k) {
+        Stack<TreeNode> s = new Stack<>();
+        TreeNode p = root;
+        while (!s.isEmpty() || p != null) {
+            if (p != null) {
+                s.push(p);
+                p = p.left;
+            } else {
+                p = s.pop();
+                if (k-- == 1) return p.val;     // error: kth starts from 1
+                p = p.right;
+            }
+        }
+        return 0;
     }
 
     // 2nd
-    public int kthSmallest(TreeNode root, int k) {
+    public int kthSmallest2(TreeNode root, int k) {
         Stack<TreeNode> stack = new Stack<>();
         TreeNode p = root;
         while (!stack.isEmpty() || p != null) {
@@ -48,7 +80,7 @@ public class Solution {
         return 0;
     }
 
-    public int kthSmallest2(TreeNode root, int k) {
+    public int kthSmallest22(TreeNode root, int k) {
         Stack<TreeNode> stack = new Stack<>();
         for (TreeNode node = root; node != null; node = node.left) {
             stack.push(node);
