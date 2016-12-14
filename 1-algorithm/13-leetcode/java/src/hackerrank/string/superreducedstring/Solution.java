8377aaf61a24889775a3fee6fef63a2b9f28b391
cdai <dc_726@163.com>
2016-12-14 15:04:41 -0500

    hackerrank: super reduced string

--- 1-algorithm/13-leetcode/java/src/hackerrank/string/superreducedstring/Solution.java
+++ 1-algorithm/13-leetcode/java/src/hackerrank/string/superreducedstring/Solution.java
@@ -9,15 +9,28 @@ public class Solution {
 
     public static void main(String[] args) {
         try (Scanner in = new Scanner(System.in)) { // Dirty (realworld) work: deal with input and output
-            String reduced = reduce(new StringBuilder(in.next())).toString();
+//            String reduced = reduce(new StringBuilder(in.next())).toString();
+            String reduced = reduce(in.next());
             System.out.println(reduced.isEmpty() ? "Empty String" : reduced);
         }
     }
 
-    private static StringBuilder reduce(StringBuilder str) {
+    // O(N) time by using StringBuilder as Stack
+    private static String reduce(String str) {
+        StringBuilder s = new StringBuilder();
+        for (char c : str.toCharArray())
+            if (s.length() > 0 && s.charAt(s.length() - 1) == c)
+                s.deleteCharAt(s.length() - 1);
+            else
+                s.append(c);
+        return s.toString();
+    }
+
+    // O(N^2) time
+    private static StringBuilder reduce2(StringBuilder str) {
         for (int i = 0; i < str.length() - 1; i++) /* safe to access i and i+1 */
             if (str.charAt(i) == str.charAt(i + 1))
-                return reduce(str.delete(i, i + 2));
+                return reduce2(str.delete(i, i + 2));
         return str;
     }
 
