a3ab30f482b566d23d0dd270e40cc91f77e3c566
cdai <dc_726@163.com>
2016-12-15 16:19:09 -0500

    hackerrank: counting sort

---
+++ 1-algorithm/13-leetcode/java/src/hackerrank/sorting/counting/Solution1.java
@@ -0,0 +1,31 @@
+package hackerrank.sorting.counting;
+
+import java.util.Scanner;
+
+/**
+ */
+public class Solution1 {
+
+    public static void main(String[] args) {
+        try (Scanner in = new Scanner(System.in)) {
+            int total = Integer.parseInt(in.nextLine());
+            int[] nums = new int[total];
+            for (int i = 0; i < total; i++)
+                nums[i] = in.nextInt();
+
+            int[] cnt = count(nums, 100);
+
+            for (int c : cnt) // Arrays.toString() print with bracket and comma
+                System.out.print(c + " ");
+            System.out.println();
+        }
+    }
+
+    private static int[] count(int[] nums, int max) { // max is exclusive
+        int[] cnt = new int[max];
+        for (int num : nums)
+            cnt[num]++;
+        return cnt;
+    }
+
+}
