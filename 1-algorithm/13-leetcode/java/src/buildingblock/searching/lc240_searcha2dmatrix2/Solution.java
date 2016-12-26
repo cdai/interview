d5844887d2114f8c2a625b8a91c2ce1c41109e78
cdai <dc_726@163.com>
2016-12-26 17:18:11 -0500

    leetcode-240: search a 2D matrix 2

--- 1-algorithm/13-leetcode/java/src/buildingblock/searching/lc240_searcha2dmatrix2/Solution.java
+++ 1-algorithm/13-leetcode/java/src/buildingblock/searching/lc240_searcha2dmatrix2/Solution.java
@@ -1,4 +1,4 @@
-package advanced.scan.twopointers.lc240_searcha2dmatrix2;
+package buildingblock.searching.lc240_searcha2dmatrix2;
 
 import java.util.Arrays;
 
@@ -24,10 +24,26 @@ public class Solution {
         System.out.println(new Solution().searchMatrix(new int[][]{{-1, 3}}, 1));
     }
 
+    // O(M+N) time. (Like how many nodes you went through in BST)
+    // I found this matrix is like a Min/Max Heap if you start from 0,0 or m,n and
+    // the only thing you know is current number is greater or smaller than both left and right "child".
+    // But you don't know how to make decision of where to go.
+    // However, if you start from m,0 or 0,n, the matrix looks like a Binary Search Tree.
+    // Then the path of search is just like to traverse the BST until we reach the target node.
+    public boolean searchMatrix(int[][] A, int target) {
+        if (A.length == 0 || A[0].length == 0) return false;
+        for (int i = A.length - 1, j = 0; i >= 0 && j < A[0].length; ) {
+            if (A[i][j] == target) return true;
+            if (A[i][j] > target) i--;
+            else j++;
+        }
+        return false;
+    }
+
     // My 2AC: O(M+N) time
     // Integers in each row are sorted in ascending from left to right.
     // Integers in each column are sorted in ascending from top to bottom.
-    public boolean searchMatrix(int[][] matrix, int target) {
+    public boolean searchMatrix2(int[][] matrix, int target) {
         if (matrix.length == 0 || matrix[0].length == 0) {
             return false;
         }
