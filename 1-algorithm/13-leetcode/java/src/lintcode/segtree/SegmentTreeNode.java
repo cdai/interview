cdffdfbcd0ac7e18f8c0630f97c0ea9b6aaf5bc2
cdai <dc_726@163.com>
2016-12-28 15:03:44 -0500

    lintcode: segment tree query

--- 1-algorithm/13-leetcode/java/src/lintcode/segtree/SegmentTreeNode.java
+++ 1-algorithm/13-leetcode/java/src/lintcode/segtree/SegmentTreeNode.java
@@ -4,11 +4,15 @@ package lintcode.segtree;
  */
 public class SegmentTreeNode {
 
-    public int start, end;
+    public int start, end, max;
 
     public SegmentTreeNode left, right;
 
     public SegmentTreeNode(int start, int end) {
+        this(start, end, 0);
+    }
+
+    public SegmentTreeNode(int start, int end, int max) {
         this.start = start;
         this.end = end;
         this.left = this.right = null;
--- 1-algorithm/13-leetcode/java/src/lintcode/segtree/Solution.java
+++ 1-algorithm/13-leetcode/java/src/lintcode/segtree/Solution.java
@@ -9,6 +9,7 @@ public class Solution {
         print(sol.build(0, 3));
     }
 
+    // Segment Tree Build I
     public SegmentTreeNode build(int start, int end) {
         if (start > end) return null; // [10,4]
 
@@ -18,6 +19,16 @@ public class Solution {
             root.right = build((start + end) / 2 + 1, end);
         }
         return root;
+    }
+
+    // Segment Tree Query I
+    public int query(SegmentTreeNode root, int start, int end) {
+        if (root.start == root.end) return root.max; // no child
+        int mid = root.start + (root.end - root.start) / 2;
+        if (end <= mid) return query(root.left, start, end);
+        if (mid < start) return query(root.right, start, end);
+        return Math.max(query(root.left, start, end),
+                query(root.right, start, end));
     }
 
     private static void print(SegmentTreeNode root) {
