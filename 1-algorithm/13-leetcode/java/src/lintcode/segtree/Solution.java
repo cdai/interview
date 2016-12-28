772fdd89129ba24f073cb91e233008f4fd8e7fd9
cdai <dc_726@163.com>
2016-12-28 15:27:49 -0500

    lintcode: segment tree build 2

--- 1-algorithm/13-leetcode/java/src/lintcode/segtree/Solution.java
+++ 1-algorithm/13-leetcode/java/src/lintcode/segtree/Solution.java
@@ -21,6 +21,37 @@ public class Solution {
         return root;
     }
 
+    // Segment Tree Build II
+    public SegmentTreeNode build(int[] A) {
+        if (A == null || A.length == 0) return null;
+        return build(A, 0, A.length - 1);
+    }
+
+    private SegmentTreeNode build(int[] A, int start, int end) {
+        if (start == end) return new SegmentTreeNode(start, end, A[start]);
+
+        int mid = start + (end - start) / 2;
+        SegmentTreeNode root = new SegmentTreeNode(start, end, 0);
+        root.left = build(A, start, mid);
+        root.right = build(A, mid + 1, end);
+        root.max = Math.max(root.left.max, root.right.max); // revise from bottom subtree max
+        return root;
+    }
+
+    private SegmentTreeNode build2(int[] A, int start, int end) {
+        if (start == end) return new SegmentTreeNode(start, end, A[start]);
+
+        int max = Integer.MIN_VALUE; // Not necessary to do this!!!
+        for (int i = start; i <= end; i++)
+            if (A[i] > max) max = A[i];
+
+        SegmentTreeNode root = new SegmentTreeNode(start, end, max);
+        int mid = start + (end - start) / 2;
+        root.left = build(A, start, mid);
+        root.right = build(A, mid + 1, end);
+        return root;
+    }
+
     // Segment Tree Query I
     public int query(SegmentTreeNode root, int start, int end) {
         if (root.start == root.end) return root.max; // no child
