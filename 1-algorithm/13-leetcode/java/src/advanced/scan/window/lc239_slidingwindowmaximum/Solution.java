a03bdca56debe2d148b2fecaeb79f19a40df8548
cdai <dc_726@163.com>
2016-12-30 17:44:15 -0500

    leetcode-239: sliding window maximum

--- 1-algorithm/13-leetcode/java/src/advanced/scan/window/lc239_slidingwindowmaximum/Solution.java
+++ 1-algorithm/13-leetcode/java/src/advanced/scan/window/lc239_slidingwindowmaximum/Solution.java
@@ -1,11 +1,39 @@
 package advanced.scan.window.lc239_slidingwindowmaximum;
 
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.LinkedList;
+
 /**
  */
 public class Solution {
 
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(Arrays.toString(sol.maxSlidingWindow(new int[]{1, 3, -1, -3, 5, 3, 6, 7}, 3)));
+    }
+
+    // O(N) amortized. Only keep max and in-window number in the deque.
     public int[] maxSlidingWindow(int[] nums, int k) {
-        return null;
+        if (nums.length == 0 || k <= 0) return new int[0];
+        Deque<Integer> q = new LinkedList<>();
+        int[] max = new int[nums.length - k + 1];
+        for (int i = 0, j = 0; i < nums.length; i++) {
+            //print(i, nums, q);
+            while (!q.isEmpty() && q.peek() < i - k + 1) q.poll(); // remove elt out of win from front
+            while (!q.isEmpty() && nums[q.peekLast()] < nums[i]) q.pollLast(); // remove smaller elt from back
+            q.offer(i);
+            if (i >= k - 1) max[j++] = nums[q.peek()];
+        }
+        return max;
+    }
+
+    private void print(int i, int[] nums, Deque<Integer> q) {
+        System.out.printf("i=%d q=[ ", i);
+        for (int j : q) {
+            System.out.printf("%d ", nums[j]);
+        }
+        System.out.printf("]\n");
     }
 
 }
