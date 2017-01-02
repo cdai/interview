700b0fb17f229726ef301ba157f50216f1e1d364
cdai <dc_726@163.com>
2017-01-02 17:03:13 -0500

    leetcode-326: power of three

--- 1-algorithm/13-leetcode/java/src/miscellaneous/math/number/lc326_powerofthree/Solution.java
+++ 1-algorithm/13-leetcode/java/src/miscellaneous/math/number/lc326_powerofthree/Solution.java
@@ -6,10 +6,21 @@ package miscellaneous.math.number.lc326_powerofthree;
  */
 public class Solution {
 
+    // Normal idea
+    public boolean isPowerOfThree(int n) {
+        if (n <= 0) return false;
+        while (n % 3 == 0) n /= 3;
+        return n == 1;
+    }
+
+    public boolean isPowerOfThree_trick(int n) {
+        return n > 0 && Math.pow(3, 19) % n == 0;
+    }
+
     // Solution from leetcode discuss
     // For 32-bit, 1162261467 is 3^19,  3^20 is bigger than int
     // 3^19 % 3^N = 0. Eg. 3^19 % 27 = 3^16 * 3^3 % 3^3 = 0
-    public boolean isPowerOfThree(int n) {
+    public boolean isPowerOfThree2(int n) {
         if (n <= 0) {
             return false;
         }
