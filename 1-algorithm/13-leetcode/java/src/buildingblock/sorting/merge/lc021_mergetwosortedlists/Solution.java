aed79e61631ec88e109930811441dc1eeb47dd4f
cdai <dc_726@163.com>
2017-01-04 20:56:32 -0500

    leetcode-21: merge two sorted lists

--- 1-algorithm/13-leetcode/java/src/buildingblock/sorting/merge/lc021_mergetwosortedlists/Solution.java
+++ 1-algorithm/13-leetcode/java/src/buildingblock/sorting/merge/lc021_mergetwosortedlists/Solution.java
@@ -8,6 +8,22 @@ import fundamentals.list.ListNode;
  */
 public class Solution {
 
+    // 3AC
+    public ListNode mergeTwoLists3(ListNode l1, ListNode l2) {
+        ListNode dmy = new ListNode(0), p = dmy;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                p = p.next = l1;
+                l1 = l1.next;
+            } else {
+                p = p.next = l2;
+                l2 = l2.next;
+            }
+        }
+        p.next = (l1 != null) ? l1 : l2;
+        return dmy.next;
+    }
+
     // Recursive solution
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         if (l1 == null || l2 == null)
