071e8613fb83856c4f25d5949038495a477d1f47
cdai <dc_726@163.com>
2017-01-06 14:39:16 -0500

    leetcode-28: implement strStr

--- 1-algorithm/13-leetcode/java/src/fundamentals/string/search/lc028_implementstrstr/Solution.java
+++ 1-algorithm/13-leetcode/java/src/fundamentals/string/search/lc028_implementstrstr/Solution.java
@@ -1,13 +1,56 @@
 package fundamentals.string.search.lc028_implementstrstr;
 
+import java.util.Arrays;
+
 /**
  * Returns the index of the first occurrence of needle in haystack,
  * or -1 if needle is not part of haystack.
  */
 public class Solution {
 
-    // My 3AC: O(MN) time
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.strStr("abcababdababaff", "ababa"));
+    }
+
     public int strStr(String s, String t) {
+        int[] f = computePrefixFunc(t);
+        System.out.println(Arrays.toString(f));
+        int i, j;
+        for (i = 0, j = 0; i < s.length() && j < t.length(); i++) { // never back up i
+            while (j > 0 && s.charAt(i) != t.charAt(j)) j = f[j - 1]; // back up j recursively until next char matched
+            if (s.charAt(i) == t.charAt(j)) j++; // if matched move j, otherwise give up current i and move onto next
+        }
+        return j == t.length() ? i - j : -1;
+    }
+
+    // Similar to strStr except compare t against itself
+    private int[] computePrefixFunc(String t) {
+        int[] f = new int[t.length()];
+        for (int i = 1, j = 0; i < t.length(); i++) { // now i = #matched
+            while (j > 0 && t.charAt(i) != t.charAt(j)) j = f[j - 1];
+            if (t.charAt(i) == t.charAt(j)) j++;
+            f[i] = j;
+        }
+        return f;
+    }
+
+    // 4AC. Brute force from <Algorithm,4e>. Prepare for KMP.
+    public int strStr4(String s, String t) {
+        if (s == null || t == null) return -1;
+        int i, j;
+        for (i = 0, j = 0; i < s.length() && j < t.length(); i++) {
+            if (s.charAt(i) == t.charAt(j)) j++;
+            else { // backup i and j
+                i -= j;
+                j = 0;
+            }
+        }
+        return j == t.length() ? i - j : -1;
+    }
+
+    // My 3AC: O(MN) time
+    public int strStr3(String s, String t) {
         if (t.isEmpty()) return 0; // error1: "",""=>0  "a",""=>0
         for (int i = 0; i <= s.length() - t.length(); i++) { // error2: <=
             for (int j = 0; j < t.length(); j++) {
