d3c3ee8ad1604fab637d9b3fb0edb5834e3bf618
cdai <dc_726@163.com>
2017-01-09 17:37:28 -0500

    leetcode-88: merge sorted array

--- 1-algorithm/13-leetcode/java/src/buildingblock/sorting/merge/lc088_mergesortedarray/Solution.java
+++ 1-algorithm/13-leetcode/java/src/buildingblock/sorting/merge/lc088_mergesortedarray/Solution.java
@@ -7,9 +7,15 @@ package buildingblock.sorting.merge.lc088_mergesortedarray;
  */
 public class Solution {
 
+    public void merge(int[] A, int m, int[] B, int n) {
+        for (int i = m - 1, j = n - 1, k = m + n - 1; j >= 0; k--) { // terminate if A empty
+            A[k] = (i >= 0 && A[i] > B[j]) ? A[i--] : B[j--]; // Pick larger from B or A is empty
+        }
+    }
+
     // My 3AC. O(max(M,N)) time. Terminate early if nums2 reach end.
     // Otherwise, pick nums2 if nums1 reach end or nums2 is larger.
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
+    public void merge3(int[] nums1, int m, int[] nums2, int n) {
         for (int i = m - 1, j = n - 1, k = m + n - 1; k >= 0 && j >= 0; k--)
             nums1[k] = (i < 0 || nums1[i] < nums2[j]) ? nums2[j--] : nums1[i--];
     }
