b1b926cc7c110f5006f1bd8985257bbdd734dfa5
cdai <dc_726@163.com>
2017-01-09 17:34:43 -0500

    leetcode-79: word search

--- 1-algorithm/13-leetcode/java/src/advanced/combinatorial/backtracking/dfs/lc079_wordsearch/Solution.java
+++ 1-algorithm/13-leetcode/java/src/advanced/combinatorial/backtracking/dfs/lc079_wordsearch/Solution.java
@@ -17,9 +17,31 @@ package advanced.combinatorial.backtracking.dfs.lc079_wordsearch;
  */
 public class Solution {
 
+    // 4AC
+    public boolean exist(char[][] board, String word) {
+        for (int i = 0; i < board.length; i++)
+            for (int j = 0; j < board[i].length; j++)
+                if (dfs(board, word, i, j, 0)) return true;
+        return false;
+    }
+
+    private boolean dfs(char[][] board, String word, int x, int y, int i) {
+        if (i == word.length()) return true;
+        if (x < 0 || x >= board.length || y < 0 || y >= board[0].length) return false;
+        if (board[x][y] != word.charAt(i)) return false;
+
+        board[x][y] = ' ';
+        boolean ret = dfs(board, word, x + 1, y, i + 1) ||
+                dfs(board, word, x - 1, y, i + 1) ||
+                dfs(board, word, x, y + 1, i + 1) ||
+                dfs(board, word, x, y - 1, i + 1);
+        board[x][y] = word.charAt(i);
+        return ret;
+    }
+
     // My 3AC. Avoid substring() cost.
     // O(mn*mn) or more precisely O(mn*4^len(w)).
-    public boolean exist(char[][] board, String word) {
+    public boolean exist3(char[][] board, String word) {
         if (board.length == 0 || board[0].length == 0) return word.isEmpty();
         for (int i = 0; i < board.length; i++)
             for (int j = 0; j < board[i].length; j++)
