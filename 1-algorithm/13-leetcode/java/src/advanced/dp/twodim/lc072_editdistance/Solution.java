a1f764f1bd63ff75971087ada7e596e15152df9a
cdai <dc_726@163.com>
2017-01-11 16:48:07 -0500

    lintcode-72: edit distance

--- 1-algorithm/13-leetcode/java/src/advanced/dp/twodim/lc072_editdistance/Solution.java
+++ 1-algorithm/13-leetcode/java/src/advanced/dp/twodim/lc072_editdistance/Solution.java
@@ -14,13 +14,31 @@ public class Solution {
         System.out.println(new Solution().minDistance("apple", "are"));
     }
 
+    // dp[i][j] = min distance between word1[0,i) and word2[0,j)
+    public int minDistance(String word1, String word2) {
+        int m = word1.length(), n = word2.length();
+        if (m == 0 || n == 0) return m == 0 ? n : m;
+
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) dp[i][0] = i; // dist = #char since word2[0,0)
+        for (int j = 1; j <= n; j++) dp[0][j] = j;
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                dp[i][j] = Math.min(Math.min(dp[i - 1][j] + 1, dp[i][j - 1] + 1),
+                        dp[i - 1][j - 1] + (word1.charAt(i - 1) == word2.charAt(j - 1) ? 0 : 1));
+            }
+        }
+        return dp[m][n];
+    }
+
     // O(N^2) time.
     // Prefix approach: dp[i][j] = w1[1:i] and w2[1:j] min dist
     // Link with smaller problem:
     //      1.delete w1[i] => dp[i-1][j] + 1
     //      2.delete w2[j] => dp[i][j-1] + 1
     //      3.substitute i to j if diff: dp[i-1][j-1] + 1
-    public int minDistance(String word1, String word2) {
+    public int minDistance2(String word1, String word2) {
         int[][] dp = new int[word1.length() + 1][word2.length() + 1];
         for (int i = 1; i <= word1.length(); i++)
             dp[i][0] = i;
