6c3c70b9a5c07fe682c7c06e062ab5eb4be9a61c
cdai <dc_726@163.com>
2017-01-12 17:43:21 -0500

    leetcode-334: increasing triplet subsequence

--- 1-algorithm/13-leetcode/java/src/advanced/scan/sequence/lc334_increasingtripletsubsequence/Solution.java
+++ 1-algorithm/13-leetcode/java/src/advanced/scan/sequence/lc334_increasingtripletsubsequence/Solution.java
@@ -13,6 +13,19 @@ import java.util.Arrays;
  */
 public class Solution {
 
+    // min1 remembers some number. min2 remembers some number > min1 (not necessarily current value of min1. Key!)
+    // 2,4,0,2,4 or 2,4,0,5 (at 0, don't clean 2,4 or discard 0, both wrong. Simply set min1=0. min2=4 still).
+    // If min2 be updated later so that old min1 and min2 discarded, it's ok, since both min1,min2<old, don't affect if min3 exists.
+    public boolean increasingTriplet(int[] nums) {
+        int min1 = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE;
+        for (int num : nums) {
+            if (num <= min1) min1 = num;        /* num <= min1 */
+            else if (num <= min2) min2 = num;   /* min1 < num */
+            else return true;                   /* min1 < min2 < num */
+        }
+        return false;
+    }
+
     // A general K version
     public boolean increasingTriplet_general(int[] nums) {
         int[] triplet = new int[3];
@@ -29,7 +42,7 @@ public class Solution {
 
     // My 2nd: it's still very hard to come up...
     // My 1st
-    public boolean increasingTriplet(int[] nums) {
+    public boolean increasingTriplet2(int[] nums) {
         int t1 = Integer.MAX_VALUE, t2 = Integer.MAX_VALUE;
         for (int num : nums) {
             if (num <= t1) {        // num <= t1 ("eat" duplicate to avoid it become t2 or even return true)
