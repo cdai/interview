22d119eac4f4f92bf651759b5656e2cbd6db9322
cdai <dc_726@163.com>
2017-01-14 11:54:03 -0500

    leetcode-129: sum root to leaf numbers

--- 1-algorithm/13-leetcode/java/src/fundamentals/tree/dfs/leaf/lc129_sumroottoleafnumbers/Solution.java
+++ 1-algorithm/13-leetcode/java/src/fundamentals/tree/dfs/leaf/lc129_sumroottoleafnumbers/Solution.java
@@ -11,9 +11,20 @@ import java.util.LinkedList;
  */
 public class Solution {
 
+    public int sumNumbers(TreeNode root) {
+        return dfs(root, 0);
+    }
+
+    private int dfs(TreeNode root, int num) {
+        if (root == null) return 0; // null root or missing child of non-leaf node
+        num = num * 10 + root.val;
+        if (root.left == null && root.right == null) return num;
+        return dfs(root.left, num) + dfs(root.right, num);
+    }
+
     // My 2nd: much faster by using int as path
     //          no need to recover it like LinkedList.
-    public int sumNumbers(TreeNode root) {
+    public int sumNumbers2(TreeNode root) {
         return doSum(root, 0);
     }
 
