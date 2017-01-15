afd9962c139f698f2efa5a11f399492e8881cbe7
cdai <dc_726@163.com>
2017-01-14 20:08:51 -0500

    lintcode-139: subarray sum closest

--- 1-algorithm/13-leetcode/java/src/lintcode/scan/Solution.java
+++ 1-algorithm/13-leetcode/java/src/lintcode/scan/Solution.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 /**
  */
@@ -11,6 +12,7 @@ public class Solution {
 
     public static void main(String[] args) {
         System.out.println(new Solution().subarraySum(new int[]{-3, 1, 2, -3, 4}));
+        System.out.println(Arrays.toString(new Solution().subarraySumClosest(new int[]{-3, 1, 1, -3, -5})));
     }
 
     // 138-Subarray Sum
@@ -25,6 +27,29 @@ public class Solution {
             presum.put(sum, i);
         }
         return Arrays.asList(-1, -1);
+    }
+
+    // 139-Subarray Sum Closest
+    // Alternative:
+    // 1) Binary search is not working since orginal cumulative array is disorder (negative num possibly)
+    // 2) Sort cumulative array then find most closest two is ok, but index is missing (extra Pair class required)
+    public int[] subarraySumClosest(int[] nums) {
+        TreeMap<Integer, Integer> presum = new TreeMap<>();
+        presum.put(0, -1);
+        int[] idx = new int[2];
+        for (int i = 0, sum = 0, min = Integer.MAX_VALUE; i < nums.length; i++) {
+            sum += nums[i];
+            Map.Entry[] entries = { presum.floorEntry(sum), presum.ceilingEntry(sum) };
+            for (Map.Entry<Integer, Integer> e : entries) {
+                if (e != null && Math.abs(sum - e.getKey()) < min) {
+                    min = Math.abs(sum - e.getKey()); // sum-key=num closer to zero
+                    idx[0] = e.getValue() + 1;
+                    idx[1] = i;
+                }
+            }
+            presum.put(sum, i);
+        }
+        return idx;
     }
 
 }
