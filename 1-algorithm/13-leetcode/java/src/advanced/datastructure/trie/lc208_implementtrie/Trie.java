44aa2cb1c7603a4217d4a462ba5c21dcd84fadcb
cdai <dc_726@163.com>
2017-01-15 20:56:59 -0500

    lintcode-208: implement trie

--- 1-algorithm/13-leetcode/java/src/advanced/datastructure/trie/lc208_implementtrie/Trie.java
+++ 1-algorithm/13-leetcode/java/src/advanced/datastructure/trie/lc208_implementtrie/Trie.java
@@ -1,57 +1,47 @@
 package advanced.datastructure.trie.lc208_implementtrie;
 
+/**
+ */
 public class Trie {
-
-    public static void main(String[] args) {
-        Trie trie = new Trie();
-        trie.insert("hellotrie");
-        System.out.println(trie.search("trie"));
-        System.out.println(trie.search("hello"));
-        System.out.println(trie.search("hellotrie"));
-        System.out.println(trie.startsWith("hell"));
-    }
 
     private TrieNode root;
 
-    public Trie() {
-        root = new TrieNode();
-    }
+    public Trie() { root = new TrieNode(); }
 
-    // Inserts a word into the trie.
     public void insert(String word) {
-        TrieNode cur = root;
-        for (char c : word.toCharArray()) {
-            if (cur.children[c - 'a'] == null) { // error1: create only if null
-                cur.children[c - 'a'] = new TrieNode(c);
-            }
-            cur = cur.children[c - 'a'];
-        }
-        cur.isWord = true;
+        TrieNode node = search(word, true);
+        node.isWord = true; /* node cannot be null */
     }
 
-    // Returns if the word is in the trie.
     public boolean search(String word) {
-        TrieNode cur = root;
-        for (char c : word.toCharArray()) {
-            cur = cur.children[c - 'a'];
-            if (cur == null) {
-                return false;
-            }
-        }
-        return cur.isWord;
+        TrieNode node = search(word, false);
+        return node != null && node.isWord;
     }
 
-    // Returns if there is any word in the trie
-    // that starts with the given prefix.
     public boolean startsWith(String prefix) {
-        TrieNode cur = root;
-        for (char c : prefix.toCharArray()) {
-            cur = cur.children[c - 'a'];
-            if (cur == null) {
-                return false;
+        TrieNode node = search(prefix, false);
+        return node != null; // no need to do dfs search here
+    }
+
+    private TrieNode search(String word, boolean isCreate) {
+        TrieNode node = root; /* invariant: node correspond to word[i-1] => entire word when terminate */
+        for (int i = 0; i < word.length(); i++) {
+            int idx = word.charAt(i) - 'a';
+            if (node.children[idx] == null) {
+                if (isCreate) node.children[idx] = new TrieNode();
+                else return null;
             }
+            node = node.children[idx];
         }
-        return true; // error: no need to find one non-null child
+        return node;
+    }
+
+    private boolean dfs(TrieNode node) {
+        if (node == null) return false;
+        if (node.isWord) return true;
+        for (TrieNode n : node.children)
+            if (dfs(n)) return true;
+        return false;
     }
 
 }
---
+++ 1-algorithm/13-leetcode/java/src/advanced/datastructure/trie/lc208_implementtrie/Trie2.java
@@ -0,0 +1,57 @@
+package advanced.datastructure.trie.lc208_implementtrie;
+
+public class Trie2 {
+
+    public static void main(String[] args) {
+        Trie2 trie = new Trie2();
+        trie.insert("hellotrie");
+        System.out.println(trie.search("trie"));
+        System.out.println(trie.search("hello"));
+        System.out.println(trie.search("hellotrie"));
+        System.out.println(trie.startsWith("hell"));
+    }
+
+    private TrieNode root;
+
+    public Trie2() {
+        root = new TrieNode();
+    }
+
+    // Inserts a word into the trie.
+    public void insert(String word) {
+        TrieNode cur = root;
+        for (char c : word.toCharArray()) {
+            if (cur.children[c - 'a'] == null) { // error1: create only if null
+                cur.children[c - 'a'] = new TrieNode(c);
+            }
+            cur = cur.children[c - 'a'];
+        }
+        cur.isWord = true;
+    }
+
+    // Returns if the word is in the trie.
+    public boolean search(String word) {
+        TrieNode cur = root;
+        for (char c : word.toCharArray()) {
+            cur = cur.children[c - 'a'];
+            if (cur == null) {
+                return false;
+            }
+        }
+        return cur.isWord;
+    }
+
+    // Returns if there is any word in the trie
+    // that starts with the given prefix.
+    public boolean startsWith(String prefix) {
+        TrieNode cur = root;
+        for (char c : prefix.toCharArray()) {
+            cur = cur.children[c - 'a'];
+            if (cur == null) {
+                return false;
+            }
+        }
+        return true; // error: no need to find one non-null child
+    }
+
+}
