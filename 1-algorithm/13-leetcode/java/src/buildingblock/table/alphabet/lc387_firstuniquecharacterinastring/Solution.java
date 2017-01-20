719018837e7887fb6cbe512f7b610528f40f9310
cdai <dc_726@163.com>
2017-01-20 11:31:37 -0500

    leetcode-387: first unique character in a string

--- 1-algorithm/13-leetcode/java/src/buildingblock/table/alphabet/lc387_firstuniquecharacterinastring/Solution.java
+++ 1-algorithm/13-leetcode/java/src/buildingblock/table/alphabet/lc387_firstuniquecharacterinastring/Solution.java
@@ -1,5 +1,8 @@
 package buildingblock.table.alphabet.lc387_firstuniquecharacterinastring;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Given a string, find the first non-repeating character in it and return it's index.
  * If it doesn't exist, return -1.
@@ -10,7 +13,17 @@ package buildingblock.table.alphabet.lc387_firstuniquecharacterinastring;
  */
 public class Solution {
 
+    // Very straightforward and elegant O(N) time and O(1) space solution
     public int firstUniqChar(String s) {
+        int[] freq = new int[26];
+        for (int i = 0; i < s.length(); i++) freq[s.charAt(i) - 'a']++;
+        for (int i = 0; i < s.length(); i++) {
+            if (freq[s.charAt(i) - 'a'] == 1) return i;
+        }
+        return -1;
+    }
+
+    public int firstUniqChar1(String s) {
         int[] letters = new int[26];
         for (char c : s.toCharArray()) {
             letters[c - 'a']++;
@@ -22,6 +35,22 @@ public class Solution {
             }
         }
         return -1;
+    }
+
+    // Not straightforward and waste space
+    public int firstUniqChar21(String s) {
+        if (s.isEmpty()) return -1;
+
+        Map<Character, Integer> idx = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (idx.containsKey(c)) idx.put(c, Integer.MAX_VALUE);
+            else idx.put(c, i);
+        }
+
+        int min = Integer.MAX_VALUE;
+        for (int i : idx.values()) min = Math.min(min, i);
+        return min == Integer.MAX_VALUE ? -1 : min;
     }
 
 }
