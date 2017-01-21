d5df88dc132068c1c87383fedf667e27a71a2de0
cdai <dc_726@163.com>
2017-01-20 22:56:56 -0500

    leetcode-10: regular expression matching

--- 1-algorithm/13-leetcode/java/src/fundamentals/string/search/lc010_regularexpressionmatching/Solution.java
+++ 1-algorithm/13-leetcode/java/src/fundamentals/string/search/lc010_regularexpressionmatching/Solution.java
@@ -28,9 +28,40 @@ public class Solution {
 //        System.out.println(new Solution().isMatch("a", ".*.."));
     }
 
+    /**
+     * f[i][j]: if s[0..i-1] matches p[0..j-1]
+     * if p[j - 1] != '*'
+     *      f[i][j] = f[i - 1][j - 1] && s[i - 1] == p[j - 1]
+     * if p[j - 1] == '*', denote p[j - 2] with x
+     *      f[i][j] is true iff any of the following is true
+     *      1) "x*" repeats 0 time and matches empty: f[i][j - 2]
+     *      2) "x*" repeats >= 1 times and matches "x*x": s[i - 1] == x && f[i - 1][j]
+     * '.' matches any single character
+     */
+    public boolean isMatch(String s, String p) {
+        int m = s.length(), n = p.length();
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[0][0] = true;
+        for (int j = 1; j < n; j += 2) // base case: s="", but p=?*?*...
+            dp[0][j + 1] = (p.charAt(j) == '*') && dp[0][j - 1];
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (p.charAt(j - 1) == '*') {/* must have j >= 2, p[0]='*' is invalid */
+                    dp[i][j] |= dp[i][j - 2] || (dp[i - 1][j] && match(s.charAt(i - 1), p.charAt(j - 2))); // star repeats 0 or >= 1 times
+                } else {
+                    dp[i][j] |= dp[i - 1][j - 1] && match(s.charAt(i - 1), p.charAt(j - 1));
+                }
+            }
+        }
+        return dp[m][n];
+    }
+
+    private boolean match(char s, char p) { return p == '.' || p == s; }
+
     // 1.Must check s.isEmpty() later, eg."a","a**" -> "", "**"
     // 2.Don't match * to zero to all, just zero with p[1..] and one with p[0..]
-    public boolean isMatch(String s, String p) {
+    public boolean isMatch3(String s, String p) {
         if (p.isEmpty()) return s.isEmpty();
         if (p.length() > 1 && p.charAt(1) == '*') {
             return isMatch(s, p.substring(2)) ||
