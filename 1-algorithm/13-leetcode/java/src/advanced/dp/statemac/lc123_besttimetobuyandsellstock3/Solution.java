a4767547b57e2e0d1afc2898674dd29399f72c43
cdai <dc_726@163.com>
2017-01-21 14:51:21 -0500

    leetcode-123: best time to buy and sell stock III

--- 1-algorithm/13-leetcode/java/src/advanced/dp/statemac/lc123_besttimetobuyandsellstock3/Solution.java
+++ 1-algorithm/13-leetcode/java/src/advanced/dp/statemac/lc123_besttimetobuyandsellstock3/Solution.java
@@ -47,6 +47,18 @@ public class Solution {
         return sell2;
     }
 
+    // Also correct
+    public int maxProfit32(int[] prices) {
+        int buy1 = Integer.MIN_VALUE, sell1 = 0, buy2 = Integer.MIN_VALUE, sell2 = 0;
+        for (int price : prices) {
+            sell2 = Math.max(sell2, buy2 + price);
+            buy2 = Math.max(buy2, sell1 - price);
+            sell1 = Math.max(sell1, buy1 + price);
+            buy1 = Math.max(buy1, -price);
+        }
+        return sell2;
+    }
+
     // My 2AC: O(N) time, two passes
     public int maxProfit2(int[] prices) {
         int n = prices.length;
