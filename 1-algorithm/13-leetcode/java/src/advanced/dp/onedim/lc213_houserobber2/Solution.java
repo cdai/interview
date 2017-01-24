38539e0a2f1c6992b29c939f5df8b6ff0665109f
cdai <dc_726@163.com>
2017-01-23 22:38:32 -0500

    leetcode-213: house robber II

--- 1-algorithm/13-leetcode/java/src/advanced/dp/onedim/lc213_houserobber2/Solution.java
+++ 1-algorithm/13-leetcode/java/src/advanced/dp/onedim/lc213_houserobber2/Solution.java
@@ -15,8 +15,25 @@ public class Solution {
         System.out.println(new Solution().rob(new int[]{3, 5, 7, 1, 12, 10}));
     }
 
-    // My 2AC: not 100% understand why it is correct. O(N) time
     public int rob(int[] nums) {
+        int n = nums.length;
+        if (n == 0) return 0;
+        if (n == 1) return nums[0];
+        return Math.max(rob(nums, 1, n), rob(nums, 0, n - 1));
+    }
+
+    private int rob(int[] nums, int start, int end) {
+        int rob = 0, notrob = 0;
+        for (int i = start; i < end; i++) {
+            int prerob = rob;
+            rob = notrob + nums[i];
+            notrob = Math.max(notrob, prerob);
+        }
+        return Math.max(rob, notrob);
+    }
+
+    // My 2AC: not 100% understand why it is correct. O(N) time
+    public int rob2(int[] nums) {
         if (nums.length == 1) return nums[0]; // error: Eg.[1] -> 0 which is wrong!
         return Math.max(
                 doRob(nums, 1, nums.length),
@@ -52,7 +69,7 @@ public class Solution {
         );
     }
 
-    private int rob(int[] nums, int from, int to) {
+    private int rob1(int[] nums, int from, int to) {
         int length = to - from + 1;
         int[] max = new int[length];
         for (int i = from; i <= to; i++) {
