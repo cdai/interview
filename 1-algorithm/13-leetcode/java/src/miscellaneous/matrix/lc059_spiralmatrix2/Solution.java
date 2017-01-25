826daaf19d187162bd8fb091ea9758d00166deee
cdai <dc_726@163.com>
2017-01-25 11:21:58 -0500

    leetcode-59: spiral matrix II

--- 1-algorithm/13-leetcode/java/src/miscellaneous/matrix/lc059_spiralmatrix2/Solution.java
+++ 1-algorithm/13-leetcode/java/src/miscellaneous/matrix/lc059_spiralmatrix2/Solution.java
@@ -12,6 +12,23 @@ package miscellaneous.matrix.lc059_spiralmatrix2;
 public class Solution {
 
     public int[][] generateMatrix(int n) {
+        if (n <= 0) return new int[0][0];
+        int[][] ret = new int[n][n];
+        int num = 1, left = 0, right = n - 1, top = 0, bottom = n - 1;
+        while (left <= right && top <= bottom) {
+            for (int i = left; i <= right; i++) ret[top][i] = num++;
+            top++;
+            for (int i = top; i <= bottom; i++) ret[i][right] = num++;
+            right--;
+            for (int i = right; i >= left; i--) ret[bottom][i] = num++;
+            bottom--;
+            for (int i = bottom; i >= top; i--) ret[i][left] = num++;
+            left++;
+        }
+        return ret;
+    }
+
+    public int[][] generateMatrix2(int n) {
         int[][] matrix = new int[n][n];
         if (n <= 0) return matrix;
 
