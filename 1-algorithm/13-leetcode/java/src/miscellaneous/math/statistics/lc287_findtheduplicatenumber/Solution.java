e5d77e08d44452c023afc4c9e2b2f7e2b25ebad8
cdai <dc_726@163.com>
2017-01-28 16:49:04 -0500

    leetcode-287: find the duplicate number

--- 1-algorithm/13-leetcode/java/src/miscellaneous/math/statistics/lc287_findtheduplicatenumber/Solution.java
+++ 1-algorithm/13-leetcode/java/src/miscellaneous/math/statistics/lc287_findtheduplicatenumber/Solution.java
@@ -12,10 +12,40 @@ package miscellaneous.math.statistics.lc287_findtheduplicatenumber;
  */
 public class Solution {
 
+    // Nice use of binary search in O(NlogN) time.
+    public int findDuplicate(int[] nums) {
+        int l = 1, r = nums.length; /* search in range number [1,n] not actual nums array */
+        while (l < r) {
+            int m = l + (r - l) / 2;
+            int cnt = 0;
+            for (int n : nums) {
+                if (n <= m) cnt++;
+            }
+            if (cnt <= m) l = m + 1;/* cnt<=m: all distinct or miss some in [1,m] caused by duplicates in [m,n] */
+            else r = m;             /* cnt>m:  duplicates in [1,m] */
+        }
+        return l;
+    }
+
+    public int findDuplicate3(int[] nums) {
+        int slow = 0, fast = 0;
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        int slow2 = 0;
+        while (slow != slow2) {
+            slow = nums[slow];
+            slow2 = nums[slow2];
+        }
+        return slow;
+    }
+
     // eg.[1,3,4,2,2]
     // mid = 2, count = 3 -> high = mid-1 = 1
     // mid = 1, count = 4 -> low = mid+1 = 2
-    public int findDuplicate(int[] nums) {
+    public int findDuplicate1(int[] nums) {
         // Numbers in nums[N] are all in [1,N-1]
         int low = 1, high = nums.length - 1;
 
