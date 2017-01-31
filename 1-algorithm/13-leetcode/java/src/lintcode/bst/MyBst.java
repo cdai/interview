d8bda9c6729e0da43296fd0dae2c29f6e96e2826
cdai <dc_726@163.com>
2017-01-30 20:50:56 -0500

    Implement my binary search tree

--- 1-algorithm/13-leetcode/java/src/lintcode/bst/MyBst.java
+++ 1-algorithm/13-leetcode/java/src/lintcode/bst/MyBst.java
@@ -17,6 +17,20 @@ public class MyBst {
         return root;
     }
 
-    
+    public TreeNode insertNode_iterative(TreeNode root, TreeNode node) {
+        TreeNode cur = root, par = null;
+        while (cur != null) {
+            par = cur;
+            cur = (node.val < cur.val) ? cur.left : cur.right;
+        }
+        if (par == null) { // root is empty
+            root = node;
+        } else if (node.val < par.val) {
+            par.left = node;
+        } else {
+            par.right = node;
+        }
+        return root;
+    }
 
 }
