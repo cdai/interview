9afff51c982c3963525cbd776e20d7dd2a9a2b7a
cdai <dc_726@163.com>
2017-01-30 21:05:13 -0500

    Implement my binary search tree

--- 1-algorithm/13-leetcode/java/src/lintcode/bst/MyBst.java
+++ 1-algorithm/13-leetcode/java/src/lintcode/bst/MyBst.java
@@ -2,6 +2,9 @@ package lintcode.bst;
 
 import fundamentals.tree.TreeNode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  */
 public class MyBst {
@@ -32,5 +35,36 @@ public class MyBst {
         }
         return root;
     }
+
+    // Leetcode-285 Inorder Successor in BST
+    // "To find a successor, you just need to find the smallest one that is larger than the given value.
+    // Since there are no duplicate values in a BST. It just like the binary search in a sorted list."
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode x) {
+        TreeNode p = root, pre = null;
+        while (p != null) { // Find lowest ancestor whose left child is ancestor of x too
+            if (x.val < p.val) {
+                pre = p;
+                p = p.left;
+            } else
+                p = p.right;
+        }
+        return pre;
+    }
+
+    // 11-Search Range in Binary Search Tree
+    public List<Integer> searchRange(TreeNode root, int k1, int k2) {
+        List<Integer> ret = new ArrayList<>();
+        dfs(ret, root, k1, k2);
+        return ret;
+    }
+
+    private void dfs(List<Integer> ret, TreeNode root, int k1, int k2) {
+        if (root == null) return;
+        if (k1 < root.val) dfs(ret, root.left, k1, k2);
+        if (k1 <= root.val && root.val <= k2) ret.add(root.val);
+        if (root.val < k2) dfs(ret, root.right, k1, k2);
+    }
+
+    // minimum, maximum, delete min, floor, ceiling, delete, rank, select refer to <Algorithm 4e>
 
 }
