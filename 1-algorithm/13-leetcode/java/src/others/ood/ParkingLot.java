fae6e5d43dc7f51cd8fc112a4df639d308761b30
cdai <dc_726@163.com>
2017-02-01 10:03:39 -0500

    OOD: parking lot

--- 1-algorithm/13-leetcode/java/src/others/ood/ParkingLot.java
+++ 1-algorithm/13-leetcode/java/src/others/ood/ParkingLot.java
@@ -8,23 +8,24 @@ enum VehicleSize {
 }
 
 abstract class Vehicle {
-    abstract boolean park(Level level);
-    abstract boolean unpark(Level level);
+    protected String plate = "";
+    public abstract boolean park(Level level);
+    public abstract boolean unpark(Level level);
 }
 
 class Motorcycle extends Vehicle {
     @Override
     public boolean park(Level level) {
-        if (level.allocate(VehicleSize.Motorcycle, 1)) return true;
-        if (level.allocate(VehicleSize.Compact, 1)) return true;
-        if (level.allocate(VehicleSize.Large, 1)) return true;
+        if (level.allocate(VehicleSize.Motorcycle, plate, 1)) return true;
+        if (level.allocate(VehicleSize.Compact, plate, 1)) return true;
+        if (level.allocate(VehicleSize.Large, plate, 1)) return true;
         return false;
     }
     @Override
     public boolean unpark(Level level) {
-        if (level.free(VehicleSize.Motorcycle, 1)) return true;
-        if (level.free(VehicleSize.Compact, 1)) return true;
-        if (level.free(VehicleSize.Large, 1)) return true;
+        if (level.free(VehicleSize.Motorcycle, plate, 1)) return true;
+        if (level.free(VehicleSize.Compact, plate, 1)) return true;
+        if (level.free(VehicleSize.Large, plate, 1)) return true;
         return false;
     }
 }
@@ -32,14 +33,14 @@ class Motorcycle extends Vehicle {
 class Car extends Vehicle {
     @Override
     public boolean park(Level level) {
-        if (level.allocate(VehicleSize.Compact, 1)) return true;
-        if (level.allocate(VehicleSize.Large, 1)) return true;
+        if (level.allocate(VehicleSize.Compact, plate, 1)) return true;
+        if (level.allocate(VehicleSize.Large, plate, 1)) return true;
         return false;
     }
     @Override
     public boolean unpark(Level level) {
-        if (level.free(VehicleSize.Compact, 1)) return true;
-        if (level.free(VehicleSize.Large, 1)) return true;
+        if (level.free(VehicleSize.Compact, plate, 1)) return true;
+        if (level.free(VehicleSize.Large, plate, 1)) return true;
         return false;
     }
 }
@@ -47,11 +48,11 @@ class Car extends Vehicle {
 class Bus extends Vehicle {
     @Override
     public boolean park(Level level) {
-        return level.allocate(VehicleSize.Large, 5);
+        return level.allocate(VehicleSize.Large, plate, 5);
     }
     @Override
     public boolean unpark(Level level) {
-        return level.free(VehicleSize.Large, 5);
+        return level.free(VehicleSize.Large, plate, 5);
     }
 }
 
@@ -59,20 +60,20 @@ class Bus extends Vehicle {
 class Level {
 
     private int m, n;
-    private boolean[][] lot;
+    private String[][] lot;
 
     Level(int rows, int spots) {
         m = rows;
         n = spots;
-        lot = new boolean[rows][spots];
+        lot = new String[rows][spots];
     }
 
-    boolean allocate(VehicleSize type, int num) {
+    boolean allocate(VehicleSize type, String plate, int num) {
         for (int i = 0; i < m; i++) {
             for (int j = start(type); j <= end(type) - num; j++) {
-                if (lot[i][j]) continue;
+                if (lot[i][j] != null) continue;
                 while (num-- > 0) {
-                    lot[i][j++] = true;
+                    lot[i][j++] = plate;
                 }
                 return true;
             }
@@ -80,12 +81,12 @@ class Level {
         return false;
     }
 
-    boolean free(VehicleSize type, int num) {
+    boolean free(VehicleSize type, String plate, int num) {
         for (int i = 0; i < m; i++) {
             for (int j = end(type) - 1; j >= start(type) + num - 1; j--) {
-                if (!lot[i][j]) continue;
+                if (!plate.equals(lot[i][j])) continue;
                 while (num-- > 0) {
-                    lot[i][j--] = false;
+                    lot[i][j--] = null;
                 }
                 return true;
             }
@@ -96,17 +97,29 @@ class Level {
     private int start(VehicleSize type) { // inclusive
         if (type == VehicleSize.Motorcycle) return 0;
         else if (type == VehicleSize.Compact) return n / 4;
-        else return n / 3 * 4;
+        else return n / 4 * 3; // ?
     }
 
     private int end(VehicleSize type) { // exclusive
         if (type == VehicleSize.Motorcycle) return n / 4;
-        else if (type == VehicleSize.Compact) return n / 3 * 4;
+        else if (type == VehicleSize.Compact) return n / 4 * 3;
         else return n;
     }
 }
 
 public class ParkingLot {
+
+    public static void main(String[] args) {
+        ParkingLot parklot = new ParkingLot(1, 1, 11);
+        parklot.parkVehicle(new Motorcycle());
+        parklot.parkVehicle(new Car());
+        parklot.parkVehicle(new Car());
+        parklot.parkVehicle(new Car());
+        parklot.parkVehicle(new Car());
+        parklot.parkVehicle(new Car());
+        parklot.parkVehicle(new Bus());
+        parklot.unParkVehicle(new Car());
+    }
 
     private Level[] levels;
 
