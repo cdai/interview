2d61918f30ed656080d39e11fb69ce27537adb10
cdai <dc_726@163.com>
2017-02-10 22:07:06 -0500

    leetcode-186: reverse words in a string II

--- 1-algorithm/13-leetcode/java/src/fundamentals/string/manipulate/lc186_reversewordsinastring2/Solution.java
+++ 1-algorithm/13-leetcode/java/src/fundamentals/string/manipulate/lc186_reversewordsinastring2/Solution.java
@@ -1,29 +1,37 @@
 package fundamentals.string.manipulate.lc186_reversewordsinastring2;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 /**
  * Given an input string, reverse the string word by word. A word is defined as a sequence of non-space characters.
  * The input string does not contain leading or trailing spaces and the words are always separated by a single space.
  */
 public class Solution {
 
-    public static void main(String[] args) {
-        char[] s = "the sky is blue".toCharArray();
-        new Solution().reverseWords(s);
-        System.out.println(String.valueOf(s));
+    @Test
+    void testNormal() {
+        char[] ch1 = "hello".toCharArray();
+        reverseWords(ch1);
+        Assertions.assertEquals("hello", String.valueOf(ch1));
+
+        char[] ch2 = "the sky is blue".toCharArray();
+        reverseWords(ch2);
+        Assertions.assertEquals("blue is sky the", String.valueOf(ch2));
     }
 
     public void reverseWords(char[] s) {
         int n = s.length;
-        swap(s, 0, n - 1);
+        reverse(s, 0, n - 1);
         for (int i = 0, j = 0; j <= n; j++) {
             if (j == n || s[j] == ' ') {
-                swap(s, i, j - 1);
+                reverse(s, i, j - 1);
                 i = j + 1;
             }
         }
     }
 
-    private void swap(char[] s, int i, int j) {
+    private void reverse(char[] s, int i, int j) {
         for (; i < j; i++, j--) {
             char tmp = s[i];
             s[i] = s[j];
