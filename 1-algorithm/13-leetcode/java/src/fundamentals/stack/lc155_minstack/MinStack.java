19fb729f5d20f15ca8095f0142bb8aa1d1f61e14
cdai <dc_726@163.com>
2017-02-11 09:50:59 -0500

    leetcode-155: min stack

--- 1-algorithm/13-leetcode/java/src/fundamentals/stack/lc155_minstack/MinStack.java
+++ 1-algorithm/13-leetcode/java/src/fundamentals/stack/lc155_minstack/MinStack.java
@@ -3,43 +3,38 @@ package fundamentals.stack.lc155_minstack;
 import java.util.Stack;
 
 /**
- * Store the gap and try to restore it upon pop()
  */
 public class MinStack {
 
-    private Stack<Long> stack;
-
+    private Stack<Long> dif = new Stack<>();
     private long min;
 
-    public MinStack() {
-        this.stack = new Stack<>();
-        this.min = Integer.MAX_VALUE;
-    }
-
     public void push(int num) {
-        if (stack.isEmpty()) {
-            stack.push(0L);
+        if (dif.isEmpty()) {
+            dif.push(0L);
             min = num;
         } else {
-            // Case-1: num is smallest => stack.push(neg-gap), (new-)min = num
-            // Case-2: num is NOT => stack.push(pos-gap), (new-)min = min
-            // neg/pos-gap = num - (old-)min
-            stack.push(num - min);
+            dif.push(num - min);
             min = Math.min(min, num);
         }
     }
 
+    // 2 Cases:
+    // dif < 0: given num==min, dif(num-lastmin) => last min
+    // dif > 0: given lastmin, dif(num-lastmin) => num
     public int pop() {
-        long gap = stack.pop();
-        if (gap > 0)
-            return (int) (min + gap);
-
-        int num = (int) min;
-        min -= gap;
-        return num;
+        long num;
+        if (dif.peek() < 0) {
+            num = min;
+            min = num - dif.pop();
+        } else {
+            num = min + dif.pop();
+        }
+        return (int) num;
     }
 
     public int min() {
         return (int) min;
     }
+
 }
---
+++ 1-algorithm/13-leetcode/java/src/fundamentals/stack/lc155_minstack/MinStack4.java
@@ -0,0 +1,45 @@
+package fundamentals.stack.lc155_minstack;
+
+import java.util.Stack;
+
+/**
+ * Store the gap and try to restore it upon pop()
+ */
+public class MinStack4 {
+
+    private Stack<Long> stack;
+
+    private long min;
+
+    public MinStack4() {
+        this.stack = new Stack<>();
+        this.min = Integer.MAX_VALUE;
+    }
+
+    public void push(int num) {
+        if (stack.isEmpty()) {
+            stack.push(0L);
+            min = num;
+        } else {
+            // Case-1: num is smallest => stack.push(neg-gap), (new-)min = num
+            // Case-2: num is NOT => stack.push(pos-gap), (new-)min = min
+            // neg/pos-gap = num - (old-)min
+            stack.push(num - min);
+            min = Math.min(min, num);
+        }
+    }
+
+    public int pop() {
+        long gap = stack.pop();
+        if (gap > 0)
+            return (int) (min + gap);
+
+        int num = (int) min;
+        min -= gap;
+        return num;
+    }
+
+    public int min() {
+        return (int) min;
+    }
+}
