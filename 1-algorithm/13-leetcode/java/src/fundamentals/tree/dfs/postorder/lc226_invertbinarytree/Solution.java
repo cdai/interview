79dc4b5618f34ce5cc0c536981169a815e60e537
cdai <dc_726@163.com>
2017-02-12 10:04:31 -0500

    leetcode-226: invert binary tree

--- 1-algorithm/13-leetcode/java/src/fundamentals/tree/dfs/postorder/lc226_invertbinarytree/Solution.java
+++ 1-algorithm/13-leetcode/java/src/fundamentals/tree/dfs/postorder/lc226_invertbinarytree/Solution.java
@@ -2,6 +2,8 @@ package fundamentals.tree.dfs.postorder.lc226_invertbinarytree;
 
 import fundamentals.tree.TreeNode;
 
+import java.util.Stack;
+
 /**
  * Invert a binary tree.
  *      4
@@ -18,8 +20,31 @@ import fundamentals.tree.TreeNode;
  */
 public class Solution {
 
-    // My 2nd: O(N) time, O(h) space
     public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+        TreeNode right = root.right;
+        root.right = invertTree(root.left);
+        root.left = invertTree(right);
+        return root;
+    }
+
+    // Also works if we replace Stack with Queue
+    public TreeNode invertTree_iterative(TreeNode root) {
+        Stack<TreeNode> s = new Stack<>();
+        if (root != null) s.push(root);
+        while (!s.isEmpty()) {
+            TreeNode node = s.pop();
+            if (node.left != null) s.push(node.left);
+            if (node.right != null) s.push(node.right);
+            TreeNode tmp = node.left;
+            node.left = node.right;
+            node.right = tmp;
+        }
+        return root;
+    }
+
+    // My 2nd: O(N) time, O(h) space
+    public TreeNode invertTree2(TreeNode root) {
         if (root == null) {
             return null;
         }
