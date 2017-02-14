58d95a1dc6a065937b29a7ca543b9433c414576f
cdai <dc_726@163.com>
2017-02-14 10:06:48 -0500

    leetcode-100: same tree

--- 1-algorithm/13-leetcode/java/src/fundamentals/tree/dfs/preorder/lc100_sametree/Solution.java
+++ 1-algorithm/13-leetcode/java/src/fundamentals/tree/dfs/preorder/lc100_sametree/Solution.java
@@ -7,9 +7,15 @@ import fundamentals.tree.TreeNode;
  * Two binary trees are considered equal if they are structurally identical and the nodes have the same value.
  */
 public class Solution {
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null || q == null) return p == q;
+        return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
     // My 2nd: preorder traversal, can implement in stack
     // My 1st
-    public boolean isSameTree(TreeNode p, TreeNode q) {
+    public boolean isSameTree2(TreeNode p, TreeNode q) {
         if (p == null || q == null) {
             return (p == null && q == null);
         }
