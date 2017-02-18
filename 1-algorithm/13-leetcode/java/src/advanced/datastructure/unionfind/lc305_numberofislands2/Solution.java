1836637baefab927cf90c790ac767e806fe50b16
cdai <dc_726@163.com>
2017-02-18 11:46:50 -0500

    leetcode-305: number of islands II

--- 1-algorithm/13-leetcode/java/src/advanced/datastructure/unionfind/lc305_numberofislands2/Solution.java
+++ 1-algorithm/13-leetcode/java/src/advanced/datastructure/unionfind/lc305_numberofislands2/Solution.java
@@ -1,18 +1,50 @@
 package advanced.datastructure.unionfind.lc305_numberofislands2;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
- * Created by cdai on 9/27/16.
  */
 public class Solution {
 
-    public int[] numIslands(char[][] map, int[] op) {
-        int[] islands = new int[op.length];
-
-        return islands;
+    @Test
+    void test() {
+        Assertions.assertEquals(Arrays.asList(1,1,1,2,2),
+                numIslands2(3, 3, new int[][]{{0,0}, {0,1}, {1,0}, {2,2}, {2,1}}));
     }
 
-    private boolean union(char[][] map, int op) {
-        return false;
+    public List<Integer> numIslands2(int m, int n, int[][] ops) {
+        List<Integer> ret = new ArrayList<>();
+        int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        int[] islands = new int[m * n];
+        Arrays.fill(islands, -1);
+
+        int cnt = 0;
+        for (int[] op : ops) {
+            int root1 = op[0] * m + op[1];
+            islands[root1] = root1;
+            cnt++;
+            for (int[] d : dirs) {
+                int x = op[0] + d[0], y = op[1] + d[1], id2 = x * m + y;
+                if (x < 0 || x >= m || y < 0 || y >= n || islands[id2] == -1) continue;
+                int root2 = find(islands, id2);
+                if (root1 != root2) {
+                    islands[root2] = root1; // or optimize: islands[root1]=root2; root1=root2;
+                    cnt--;
+                }
+            }
+            ret.add(cnt);
+        }
+        return ret;
+    }
+
+    private int find(int[] islands, int id) {
+        if (islands[id] != id) islands[id] = find(islands, islands[id]);
+        return islands[id];
     }
 
 }
