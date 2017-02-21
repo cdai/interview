a357b2b2cc422ad1c23857d1860b98caf2b8f335
cdai <dc_726@163.com>
2017-02-20 22:10:15 -0500

    Palindrome in data stream

--- 1-algorithm/13-leetcode/java/src/others/algorithm/RabinKarpAlg.java
+++ 1-algorithm/13-leetcode/java/src/others/algorithm/RabinKarpAlg.java
@@ -15,6 +15,7 @@ public class RabinKarpAlg {
         Assertions.assertEquals(2, match("abcde", "cd"));
     }
 
+    @Test
     void testPalindromeInDataStream() {
         Assertions.assertTrue(add('a'));
         Assertions.assertTrue(add('a'));
@@ -26,21 +27,35 @@ public class RabinKarpAlg {
         Assertions.assertTrue(add('a'));
     }
 
-    private StringBuilder str;
-    private int half1;
-    private int half2;
+    private StringBuilder s = new StringBuilder();
+    private final int d = 256, q = 103;
+    private long h1 = 0, h2 = 0, h = 1;
 
+    // aabaa -> aabaac: h1=aa->aab (append mid), h2=aa->aac (append last)
+    // aabaac -> aabaacd: h1=aab->aab (same), h2=aac->acd (remove new mid, append last)
     public boolean add(char c) {
-        str.append(c);
+        s.append(c);
 
         // Update hash for first half and second
+        int n = s.length();
+        if (n == 1) return true;
+        if (n == 2) {
+            h1 = s.charAt(0) % q;
+            h2 = s.charAt(1) % q;
+            return s.charAt(0) == s.charAt(1);
+        }
 
+        if (n % 2 == 0) {
+            h = (h * d) % q;
+            h1 = (h1 + h * s.charAt((n - 1) / 2)) % q; // note: append to leading digit nor trailing
+            h2 = (d * h2 + s.charAt(n - 1)) % q;
+        } else {
+            h2 = (d * (h2 - h * s.charAt(n / 2)) + s.charAt(n - 1)) % q;
+            if (h2 < 0) h2 += q;
+        }
 
         // Check if false positive
-        if (half1 == half2) {
-            for (int i = 0, j = str.length() - 1; i < j; i++, j--) {
-                if (str.charAt(i) != str.charAt(j)) return false;
-            }
+        if (h1 == h2) {
             return true;
         }
         return false;
@@ -53,7 +68,7 @@ public class RabinKarpAlg {
         final int d = 256; // d-radix, q-hash size (any large since only one entry saved)
         final long q = 127;//BigInteger.probablePrime(31, new Random()).longValue();
 
-        long h = 1; // d^(m-1) % q
+        long h = 1; // old leading digit bit: d^(n-1) % q
         for (int i = 0; i < n - 1; i++) {
             h = (h * d) % q;
         }
@@ -70,7 +85,8 @@ public class RabinKarpAlg {
                     return i - n;
                 }
             }
-            // Key: rolling hash by removing old digit and adding new high order digit
+            // Key: rolling hash by removing old leading digit
+            //      and then adding new trailing digit
             if (i < m) {
                 hs = (d * (hs - h * s.charAt(i - n)) + s.charAt(i)) % q;
                 if (hs < 0) hs += q;
