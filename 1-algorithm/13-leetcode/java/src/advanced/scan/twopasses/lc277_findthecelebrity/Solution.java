580ab20028fb40ffd144c8bcc48fab360a592587
cdai <dc_726@163.com>
2017-02-24 13:58:40 -0500

    leetcode-277: find the celebrity

--- 1-algorithm/13-leetcode/java/src/advanced/scan/twopasses/lc277_findthecelebrity/Solution.java
+++ 1-algorithm/13-leetcode/java/src/advanced/scan/twopasses/lc277_findthecelebrity/Solution.java
@@ -11,20 +11,20 @@ public class Solution {
     }
 
     public int findCelebrity(int n) {
-        int celebrity = 0;
-        for (int i = 1; i < n; i++)
-            if (knows(celebrity, i))
-                celebrity = i;
+        int celeb = 0;
+        for (int i = 1; i < n; i++) {
+            if (knows(celeb, i)) celeb = i;
+        }
 
         /* for people < celebrity, they cannot be candidate because they know someone before them
          * for people > celebrity, they cannot be candidate because celebrity knows all of them */
         for (int i = 0; i < n; i++) {
-            if (i < celebrity && (knows(celebrity, i) || !knows(i, celebrity)))
+            if (i < celeb && (knows(celeb, i) || !knows(i, celeb)))
                 return -1;
-            if (i > celebrity && !knows(i, celebrity))
+            if (i > celeb && !knows(i, celeb)) // no need to check knows(celebrity,i) again
                 return -1;
         }
-        return celebrity;
+        return celeb;
     }
 
     private boolean knows(int a, int b) {
