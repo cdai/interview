2e086ae97b41f108900c72e6f0a8f6624f7e846c
cdai <dc_726@163.com>
2017-02-25 18:18:36 -0500

    leetcode-260: single number III

--- 1-algorithm/13-leetcode/java/src/miscellaneous/bitmanipulation/lc260_singlenumber3/Solution.java
+++ 1-algorithm/13-leetcode/java/src/miscellaneous/bitmanipulation/lc260_singlenumber3/Solution.java
@@ -1,6 +1,7 @@
 package miscellaneous.bitmanipulation.lc260_singlenumber3;
 
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * Given an array of numbers nums, in which exactly two elements appear only once
@@ -18,8 +19,41 @@ public class Solution {
                 new Solution().singleNumber(new int[]{1, 2, 1, 3, 2, 5})));
     }
 
-    // My 2AC: no need to compute single2 from nums again
     public int[] singleNumber(int[] nums) {
+        // 1.xor = single1 ^ single2
+        int xor = 0;
+        for (int num : nums) xor ^= num;
+
+        // 2.Get a different bit (1-bit) bewteen two singles
+        // Two's complement: 5=0101, -5=1011 (2^3 - 011=5)
+        // xor=5 & -5 = 0001 only keep least significant 1
+        xor &= -xor;
+
+        // 3.Differeniate two singles by this 1-bit
+        int[] single = new int[2];
+        for (int num : nums) {
+            if ((num & xor) == 0) single[0] ^= num;
+            else single[1] ^= num;
+        }
+        return single;
+    }
+
+    // 3AC
+    public List<Integer> singleNumberIII(int[] A) {
+        int xor = 0;
+        for (int a : A) xor ^= a;
+
+        xor -= (xor & (xor - 1));
+        int n1 = 0, n2 = 0;
+        for (int a : A) {
+            if ((a & xor) == 0) n1 ^= a;
+            else n2 ^= a;
+        }
+        return Arrays.asList(n1, n2);
+    }
+
+    // My 2AC: no need to compute single2 from nums again
+    public int[] singleNumber2(int[] nums) {
         // 1.Get single1 ^ single2
         int two = 0;
         for (int num : nums) {
