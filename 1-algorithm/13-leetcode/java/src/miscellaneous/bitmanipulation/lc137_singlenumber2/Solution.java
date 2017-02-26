5b5674f5b79948909e3ed83b25ead87c09a1006e
cdai <dc_726@163.com>
2017-02-25 22:06:14 -0500

    leetcode-137: single number II

--- 1-algorithm/13-leetcode/java/src/miscellaneous/bitmanipulation/lc137_singlenumber2/Solution.java
+++ 1-algorithm/13-leetcode/java/src/miscellaneous/bitmanipulation/lc137_singlenumber2/Solution.java
@@ -13,7 +13,39 @@ public class Solution {
                         4, 2, 4, 4
                 })
         );
-        System.out.println(singleNumber5(new int[]{2,1,2,2,1,2,2,1,3,1,1}));
+        //System.out.println(singleNumber(new int[]{2,1,2,2,1,2,2,1,3,1,1}));
+    }
+
+    // Fundamentals of binary addition
+    //  carry = a & b
+    //  sum = a ^ b (without carry)
+    // Each bit of one and two represents:
+    // 000 -> 001 -> 010 -> 100 -> 001 ...
+    // Most significant bit (b3) is 1 only if all other bits are 1
+    // If most significant bit is 1, clean all other bits
+    public int singleNumber(int[] nums) {
+        int b1 = 0, b2 = 0, b3 = 0;
+        for (int num : nums) {
+            b2 ^= b1 & num; // b2 += carry of bit-1 + num
+            b1 ^= num;      // b1 += num
+            b3 = b1 & b2;   // b3 = carry of bit-1 + bit-2
+            b1 &= ~b3;      // clean if b3 is 1
+            b2 &= ~b3;      // clean if b3 is 1
+        }
+        return b1;
+    }
+
+    // Sum up 1 on each bit and mod 3, remaining is that single num
+    public int singleNumber4(int[] nums) {
+        int cnt = 0;
+        for (int i = 0; i < 32; i++) {
+            int sum = 0;
+            for (int num : nums) {
+                sum += (num >> i) & 1;
+            }
+            cnt |= (sum % 3) << i; // must be 0 (3's multiple) or 1
+        }
+        return cnt;
     }
 
     //    c b a num
@@ -23,7 +55,7 @@ public class Solution {
     // 3: 0 1 1  1
     // 4: 1 0 0  1
     // 0: 0 0 0  1
-    public static int singleNumber5(int[] nums) {
+    public static int singleNumber3(int[] nums) {
         int one = 0, two = 0, three = 0;
         for (int num : nums) {
             two = two ^ (one & num);
@@ -51,7 +83,7 @@ public class Solution {
 
     // Explaination of elegant solution above
     // we count each bit 1's by hand, one num one bit.
-    public int singleNumber(int[] nums) {
+    public int singleNumber1(int[] nums) {
         // Count number of 1's on every bit
         byte[] count = new byte[Integer.SIZE];
         for (int i = 0; i < nums.length; i++) {
