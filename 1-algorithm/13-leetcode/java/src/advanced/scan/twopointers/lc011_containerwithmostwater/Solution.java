fef08f48229bf8b11eba60ca96489eea59360140
cdai <dc_726@163.com>
2017-03-11 19:23:24 -0500

    leetcode-11: container with most water

--- 1-algorithm/13-leetcode/java/src/advanced/scan/twopointers/lc011_containerwithmostwater/Solution.java
+++ 1-algorithm/13-leetcode/java/src/advanced/scan/twopointers/lc011_containerwithmostwater/Solution.java
@@ -9,14 +9,28 @@ package advanced.scan.twopointers.lc011_containerwithmostwater;
 public class Solution {
 
     // My 2AC: When left=right, move whatever will be ok
+    // [1,x,x,x,x,7]: no need to calculate all [1,x] pairs since area is determined by 1
+    // [o,8,x,x,x,7]: no need to calculate all [x,7] pairs for same reason
+    // [o,8,x,x,5,o]
+    // [o,8,x,9,o,o]
+    // [o,o,7,9,o,o] - done!
+    // Same as two sum O(N) solution, eg. target=4:
+    // [1,x,x,x,x,8]: no need to check all [x,8] pairs because they're greater than 1+8
+    // [1,x,x,x,6,o]
+    // [1,x,x,2,o,o]: no need to check all [1,x] pairs because they're smaller than 1+2
+    // [o,1,x,2,o,o]
+    // [o,o,1,2,o,o]
     public int maxArea(int[] height) {
-        int water = 0;
-        for (int l = 0, r = height.length - 1; l < r; ) {
-            water = Math.max(water, (r - l) * Math.min(height[l], height[r]));
-            if (height[l] < height[r]) l++;
-            else r--;
+        int max = 0, n = height.length;
+        for (int l = 0, r = n - 1; l < r; ) {
+            max = Math.max(max, Math.min(height[l], height[r]) * (r - l));
+            if (height[l] < height[r]) {
+                l++;
+            } else {
+                r--;
+            }
         }
-        return water;
+        return max;
     }
 
     // My 1AC: O(n)
